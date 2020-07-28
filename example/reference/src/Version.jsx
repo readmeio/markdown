724491dc1c549912faffe84ef8e5de042d80eed8
Jon Ursenbach <jon@ursenba.ch>
2020-07-28 12:01:27 -0700

    chore: promoting the markdown package into a single package structure

--- example/reference/src/Version.jsx
+++
@@ -1,7 +0,0 @@
-const ReactDOM = require('react-dom');
-
-function Version({ version }) {
-  return ReactDOM.createPortal(`v${version}`, document.getElementById('oas-version'));
-}
-
-module.exports = Version;
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   testURL: 'http://localhost',
   coverageReporters: ['json', 'text', 'lcov', 'clover'], // per https://github.com/facebook/jest/issues/9396#issuecomment-573328488
@@ -11,13 +9,13 @@ module.exports = {
       statements: 90,
     },
   },
-  setupFiles: [path.join(__dirname, '/lib/enzyme')],
-  coveragePathIgnorePatterns: ['<rootDir>/webpack.*.js', '<rootDir>/src/form-components', 'dist/'],
+  setupFiles: ['<rootDir>/lib/enzyme'],
+  coveragePathIgnorePatterns: ['<rootDir>/webpack.*.js', 'dist/'],
   collectCoverageFrom: ['**/*.{js,jsx}', '!**/node_modules/**', '!jest.config.js', '!**/coverage/lcov-report/**'],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
   },
   transform: {
-    '^.+\\.jsx?$': path.join(__dirname, '/lib/babel-jest'),
+    '^.+\\.jsx?$': '<rootDir>/lib/babel-jest',
   },
 };
