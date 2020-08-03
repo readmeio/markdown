b2ac71152104f7ef75e8a80c50805e52ef8fe7ad
Rafe Goldberg <rafegoldberg@gmail.com>
2020-08-03 14:30:13 -0700

    chore: update commit conventions

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,11 +1,14 @@
-const [, , typeEnum] = require('@commitlint/config-angular').rules['type-enum'];
+const [,,typeEnum] = require('@commitlint/config-angular').rules['type-enum'];
 
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
     'header-max-length':    [0, 'always', 106],
-    'type-enum':            [2, 'always', [...typeEnum, 'chore', 'ref', 'add']],
-    'scope-case':           [0, 'always', []],
+    'type-enum':            [2, 'always', [...typeEnum, 'chore', 'ref', 'add', 'enhance', 'BUILD']],
+    'type-case':            [1, 'always', ['lower-case', 'upper-case']],
+    'type-empty':           [0],
+    'scope-empty':          [0],
+    'subject-empty':        [1, 'never'],
     'body-max-line-length': [0, 'always', 'Infinity'],
   },
 };
