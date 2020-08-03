a3dad3082f9c3917a04bbdcc49b74acd0d7b91d9
Rafe Goldberg <rafegoldberg@gmail.com>
2020-08-03 15:43:29 -0700

    revert: reapply stricter commit conventions

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,11 +4,8 @@ module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
     'header-max-length':    [0, 'always', 106],
-    'type-enum':            [2, 'always', [...typeEnum, 'chore', 'ref', 'add', 'enhance', 'BUILD']],
+    'type-enum':            [2, 'always', [...typeEnum, 'chore', 'ref', 'add', 'enhance']],
     'type-case':            [1, 'always', ['lower-case', 'upper-case']],
-    'type-empty':           [0],
-    'scope-empty':          [0],
-    'subject-empty':        [1, 'never'],
     'body-max-line-length': [0, 'always', 'Infinity'],
   },
 };
