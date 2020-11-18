729cdab31b9c31f61d8a35d06f8c35755ff65212
Kelly Joseph Price <kelly@readme.com>
2020-11-18 13:23:29 -0800

    cleanup

--- __tests__/helpers.js
+++ __tests__/helpers.js
@@ -1,15 +1,16 @@
-module.exports.silenceConsole = fn => {
-  const handlers = {
-    get: (target, prop) => {
-      if (!(prop in target) && prop in console) {
-        const spy = jest.spyOn(console, prop).mockImplementation(() => {});
-        target[prop] = spy;
-      }
+const consoleStubHandler = {
+  get: (target, prop) => {
+    if (!(prop in target) && prop in console) {
+      const spy = jest.spyOn(console, prop).mockImplementation(() => {});
+      target[prop] = spy;
+    }
 
-      return target[prop];
-    },
-  };
-  const potentialSpies = new Proxy({}, handlers);
+    return target[prop];
+  },
+};
+
+module.exports.silenceConsole = fn => {
+  const potentialSpies = new Proxy({}, consoleStubHandler);
 
   try {
     return fn(potentialSpies);
