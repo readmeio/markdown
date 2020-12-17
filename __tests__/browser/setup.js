e2b1a7105a60d5bdd1e92b689f1c622bf150933c
Kelly Joseph Price <kelly@readme.com>
2020-12-17 13:58:05 -0800

    delint

--- __tests__/browser/setup.js
+++ __tests__/browser/setup.js
@@ -1,24 +1,24 @@
 /* global page */
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
-import path from 'path';
+//import path from 'path';
 
-const kebabCase = str => str.replace(/[ .]/g, '-').replace(/['"]/g, '');
+//const kebabCase = str => str.replace(/[ .]/g, '-').replace(/['"]/g, '');
 
-async function customToMatchImageSnapshot(...args) {
-  const { pass, ...rest } = toMatchImageSnapshot.bind(this)(...args);
+//async function customToMatchImageSnapshot(...args) {
+  //const { pass, ...rest } = toMatchImageSnapshot.bind(this)(...args);
 
-  if (!pass) {
-    const { testPath, currentTestName, snapshotState } = this;
+  //if (!pass) {
+    //const { testPath, currentTestName, snapshotState } = this;
 
-    // copied from: https://github.com/americanexpress/jest-image-snapshot/blob/b82b068c6e001a2e098ac2fbde3abc55ffeb493b/src/index.js#L109
-    const counter = snapshotState._counters.get(currentTestName); // eslint-disable-line no-underscore-dangle
-    const snapshotName = kebabCase(`${path.basename(testPath)}-${currentTestName}-${counter}-ci`);
-    const snapshotPath = `${path.dirname(testPath)}/__image_snapshots__/${snapshotName}.png`;
+    //// copied from: https://github.com/americanexpress/jest-image-snapshot/blob/b82b068c6e001a2e098ac2fbde3abc55ffeb493b/src/index.js#L109
+    //const counter = snapshotState._counters.get(currentTestName); // eslint-disable-line no-underscore-dangle
+    //const snapshotName = kebabCase(`${path.basename(testPath)}-${currentTestName}-${counter}-ci`);
+    //const snapshotPath = `${path.dirname(testPath)}/__image_snapshots__/${snapshotName}.png`;
 
-    await page.screenshot({ fullPage: true, path: snapshotPath });
-  }
+    //await page.screenshot({ fullPage: true, path: snapshotPath });
+  //}
 
-  return { pass, ...rest };
-}
+  //return { pass, ...rest };
+//}
 
 expect.extend({ toMatchImageSnapshot });
