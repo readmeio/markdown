370cc0501ecae0ff463f77a1ba6c3d4a5defc75d
Kelly Joseph Price <kelly@readme.com>
2020-12-17 13:12:10 -0800

    try saving snapshots on ci

--- __tests__/browser/setup.js
+++ __tests__/browser/setup.js
@@ -1,3 +1,24 @@
+/* global page */
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import path from 'path';
 
-expect.extend({ toMatchImageSnapshot });
+const kebabCase = str => str.replace(/ /g, '-').replace(/['"]/g, '');
+
+async function customToMatchImageSnapshot(...args) {
+  const { pass, ...rest } = toMatchImageSnapshot.bind(this)(...args);
+
+  if (!pass) {
+    const { testPath, currentTestName, snapshotState } = this;
+
+    // copied from: https://github.com/americanexpress/jest-image-snapshot/blob/b82b068c6e001a2e098ac2fbde3abc55ffeb493b/src/index.js#L109
+    const counter = snapshotState._counters.get(currentTestName); // eslint-disable-line no-underscore-dangle
+    const snapshotName = kebabCase(`${path.basename(testPath)}-${currentTestName}-${counter}`);
+    const snapshotPath = `${path.dirname(testPath)}/__image_snapshots__/${snapshotName}.png`;
+
+    await page.screenshot({ fullPage: true, path: snapshotPath });
+  }
+
+  return { pass, ...rest };
+}
+
+expect.extend({ toMatchImageSnapshot: customToMatchImageSnapshot });
