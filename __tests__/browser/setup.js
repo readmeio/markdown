d2e1938b32e8f324d5b49c258a0377eaf29522e0
Kelly Joseph Price <kelly@readme.com>
2020-12-17 13:28:30 -0800

    update ci snapshot

--- __tests__/browser/setup.js
+++ __tests__/browser/setup.js
@@ -2,7 +2,7 @@
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
 import path from 'path';
 
-const kebabCase = str => str.replace(/ /g, '-').replace(/['"]/g, '');
+const kebabCase = str => str.replace(/[ .]/g, '-').replace(/['"]/g, '');
 
 async function customToMatchImageSnapshot(...args) {
   const { pass, ...rest } = toMatchImageSnapshot.bind(this)(...args);
@@ -12,7 +12,7 @@ async function customToMatchImageSnapshot(...args) {
 
     // copied from: https://github.com/americanexpress/jest-image-snapshot/blob/b82b068c6e001a2e098ac2fbde3abc55ffeb493b/src/index.js#L109
     const counter = snapshotState._counters.get(currentTestName); // eslint-disable-line no-underscore-dangle
-    const snapshotName = kebabCase(`${path.basename(testPath)}-${currentTestName}-${counter}`);
+    const snapshotName = kebabCase(`${path.basename(testPath)}-${currentTestName}-${counter}-ci`);
     const snapshotPath = `${path.dirname(testPath)}/__image_snapshots__/${snapshotName}.png`;
 
     await page.screenshot({ fullPage: true, path: snapshotPath });
