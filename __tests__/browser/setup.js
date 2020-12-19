9a784c49075e6cc446fc9686ab476fdebf1bebc0
Kelly Joseph Price <kelly@readme.com>
2020-12-18 16:34:13 -0800

    Try different ci path

--- __tests__/browser/setup.js
+++ __tests__/browser/setup.js
@@ -1,5 +1,12 @@
 import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+import path from 'path';
 
-const toMatchImageSnapshot = configureToMatchImageSnapshot({});
+const opts = {};
+
+if (process.env.CI) {
+  opts.customSnapshotsDir = path.resolve('__tests__/browser/ci/');
+}
+
+const toMatchImageSnapshot = configureToMatchImageSnapshot(opts);
 
 expect.extend({ toMatchImageSnapshot });
