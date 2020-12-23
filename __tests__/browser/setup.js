5fcda0532d7905038f44b5126da1a1685bba298f
Kelly Joseph Price <kelly@readme.com>
2020-12-23 12:13:54 -0800

    makefile typo + ccheckin snapshot

--- __tests__/browser/setup.js
+++ __tests__/browser/setup.js
@@ -1,4 +1,5 @@
 import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+import path from 'path';
 
 const opts = {
   comparisonMethod: 'ssim',
@@ -6,6 +7,11 @@ const opts = {
     ssim: 'bezkrovny',
   },
 };
+
+if (process.env.CI) {
+  opts.customSnapshotsDir = path.resolve('__tests__/browser/ci/');
+}
+
 const toMatchImageSnapshot = configureToMatchImageSnapshot(opts);
 
 expect.extend({ toMatchImageSnapshot });
