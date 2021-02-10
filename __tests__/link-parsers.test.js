cd4965ea0579e5e73b44fc8c67ed6802b7a066c2
Kelly Joseph Price <kelly@readme.com>
2021-02-10 14:55:56 -0800

    wip

---
+++ __tests__/link-parsers.test.js
@@ -0,0 +1,21 @@
+const markdown = require('../index');
+
+test('a link with label', () => {
+  expect(markdown.mdast('[link](http://www.foo.com)')).toMatchSnapshot();
+});
+
+test('a link ref', () => {
+  expect(markdown.mdast('[link]')).toMatchSnapshot();
+});
+
+test('a bracketed autoLinked url', () => {
+  expect(markdown.mdast('<http://www.google.com>')).toMatchSnapshot();
+});
+
+test('a bare autoLinked url', () => {
+  expect(markdown.mdast('http://www.googl.com')).toMatchSnapshot();
+});
+
+test('a bare autoLinked url with no protocol', () => {
+  expect(markdown.mdast('www.google.com')).toMatchSnapshot();
+});
