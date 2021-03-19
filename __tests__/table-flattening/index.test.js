ae6fd843ec2c13ed539dee33fc4ce1efd65085f8
Kelly Joseph Price <kellyjosephprice@gmail.com>
2021-03-19 15:30:41 -0700

    fix: magic tables collect all cells (#139)
    | Fixes CX-188
    |:---:
    
    ## 🧰 Changes
    
    Previously, the `tableFlattening` function was assuming that tables only had plain text. In effect, if you have any markdown formatting in a table cell, it and anything after it would not be indexed!
    
    ## 🧬 QA & Testing
    
    You can verify that the test is sane, but this really needs to be end to end
    tested.

---
+++ __tests__/table-flattening/index.test.js
@@ -0,0 +1,32 @@
+const { astToPlainText, hast } = require('../../index');
+
+describe('astToPlainText with tables', () => {
+  it('includes all cells', () => {
+    const text = `
+  | Col. A  | Col. B  | Col. C  |
+  |:-------:|:-------:|:-------:|
+  | Cell A1 | Cell B1 | Cell C1 |
+  | Cell A2 | Cell B2 | Cell C2 |
+  | Cell A3 | Cell B3 | Cell C3 |`;
+
+    expect(astToPlainText(hast(text))).toMatchInlineSnapshot(`
+      "
+
+      Col. A Col. B Col. CCell A1 Cell B1 Cell C1 Cell A2 Cell B2 Cell C2 Cell A3 Cell B3 Cell C3"
+    `);
+  });
+
+  it('includes formatted text', () => {
+    const text = `
+      | *Col. A*  | Col. *B*  |
+      |:---------:|:---------:|
+      | Cell *A1* | *Cell B1* |
+      | *Cell* A2 | *Cell* B2 |`;
+
+    expect(astToPlainText(hast(text))).toMatchInlineSnapshot(`
+      "
+
+      Col. A Col.  BCell  A1 Cell B1 Cell  A2 Cell  B2"
+    `);
+  });
+});
