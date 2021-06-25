3682c6974a4324ae07da9a86ebf1f32a377db15d
Kelly Joseph Price <kelly@readme.com>
2021-06-25 15:10:03 -0700

    delint

--- __tests__/helpers.js
+++ __tests__/helpers.js
@@ -1,11 +1,13 @@
-module.exports.silenceConsole = (prop = 'error', impl = () => {}) => fn => {
-  let spy;
+module.exports.silenceConsole =
+  (prop = 'error', impl = () => {}) =>
+  fn => {
+    let spy;
 
-  try {
-    spy = jest.spyOn(console, prop).mockImplementation(impl);
+    try {
+      spy = jest.spyOn(console, prop).mockImplementation(impl);
 
-    return fn(spy);
-  } finally {
-    spy.mockRestore();
-  }
-};
+      return fn(spy);
+    } finally {
+      spy.mockRestore();
+    }
+  };
--- processor/parse/flavored/callout.js
+++ processor/parse/flavored/callout.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line unicorn/no-unsafe-regex
-const rgx = /^> ?((?:\u2139|\u2049|\u2757|\u203C|\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])+)(?: {0,}(.+))?\n((?:>(?: .*)?\n)*)/;
+const rgx =
+  // eslint-disable-next-line unicorn/no-unsafe-regex
+  /^> ?((?:\u2139|\u2049|\u2757|\u203C|\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])+)(?: {0,}(.+))?\n((?:>(?: .*)?\n)*)/;
 
 function tokenizer(eat, value) {
   if (!rgx.test(value)) return true;
