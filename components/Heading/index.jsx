d3278d51b534f4cadf0e20856d85187c87d918d7
lambertoasghedom <asghedom.lamberto@gmail.com>
2021-07-21 14:25:59 -0700

    fix(heading): add aria-label to anchors

--- components/Heading/index.jsx
+++ components/Heading/index.jsx
@@ -17,11 +17,12 @@ function Heading({ tag, showAnchorIcons, ...props }) {
   ];
 
   if (showAnchorIcons) {
+    const headingText = props.children[1];
     children.push(
       // eslint-disable-next-line jsx-a11y/anchor-has-content
       <a
         key={`heading-anchor-icon-${props.id}`}
-        aria-label={`Skip link for ${props.children[1]}`}
+        aria-label={`Skip link to ${headingText}`}
         className="heading-anchor-icon fa fa-anchor"
         href={`#${props.id}`}
       />
