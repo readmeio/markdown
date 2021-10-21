e86c92864c94bc3ef688974eef3a90b996644f89
Rafe Goldberg <rafegoldberg@gmail.com>
2021-10-21 10:10:41 -0700

    feat: autoregister custom elem tags and attrs
    Automatically safelist tag names and "attributes" (based on prop types) of the passed custom components, instead of having to do it manually!

--- sanitize.schema.js
+++ sanitize.schema.js
@@ -6,13 +6,6 @@ sanitize.clobberPrefix = '';
 sanitize.tagNames.push('span', 'style');
 sanitize.attributes['*'].push('class', 'className', 'align', 'style');
 
-/**
- * @todo don't manually whitelist custom component attributes
- *       within the engine!
- * @todo change `link` to `href`
- */
-sanitize.attributes['tutorial-tile'] = ['backgroundColor', 'emoji', 'link', 'slug'];
-
 sanitize.tagNames.push('rdme-pin');
 
 sanitize.tagNames.push('rdme-embed');
