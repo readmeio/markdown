f2063a1e47e4acbe27a12706a6e125b7b3dd6fae
Kelly Joseph Price <kelly@readme.com>
2021-12-17 14:33:55 -0800

    feat: custom block compilers

--- processor/compile/div.js
+++ processor/compile/div.js
@@ -1,8 +1,14 @@
-/* div blocks directly alias the paragraph container; use for display only! */
 module.exports = function DivCompiler() {
   const { Compiler } = this;
   const { visitors } = Compiler.prototype;
+
   visitors.div = function compile(node) {
-    return visitors.paragraph.call(this, node);
+    const data = node.data.hProperties;
+
+    return `
+[block:${data.hName}]
+${JSON.stringify(data, null, 2)}
+[/block]
+`;
   };
 };
