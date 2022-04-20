b2cc22ec45dbe05eb7b963e8ec9a3f1bc4aab02b
thenaomiday <naomi.m.day@gmail.com>
2022-04-20 16:14:39 -0400

    fix: save images in markdown format when they don't have a width (#472)

--- processor/compile/image.js
+++ processor/compile/image.js
@@ -2,9 +2,13 @@ module.exports = function ImageCompiler() {
   const { Compiler } = this;
   const { visitors } = Compiler.prototype;
 
+  const originalImageCompiler = visitors.image;
+
   visitors.image = function compile(node, ...args) {
     if (node.data?.hProperties?.className === 'emoji') return node.title;
 
-    return visitors.figure.call(this, node, ...args);
+    if (node.data.hProperties.width) return visitors.figure.call(this, node, ...args);
+
+    return originalImageCompiler.call(this, node, ...args);
   };
 };
