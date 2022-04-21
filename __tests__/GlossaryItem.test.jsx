4ba3bfc826def4d7cc3219b10016fab423d0f792
gkoberger <gkoberger@gmail.com>
2022-04-20 20:05:03 -0400

    i should write tests

--- __tests__/GlossaryItem.test.jsx
+++ __tests__/GlossaryItem.test.jsx
@@ -12,7 +12,19 @@ test('should output a glossary item if the term exists', () => {
   expect(container.querySelector('.tooltip-content-body')).toHaveTextContent(`${term} - ${definition}`);
 });
 
-test('should output nothing if the term does not exist', () => {
-  const { container } = render(<GlossaryItem term="something" terms={[]} />);
-  expect(container).toBeEmptyDOMElement();
+test('should be case insensitive', () => {
+  const term = 'aCme';
+  const definition = 'This is a definition';
+  const { container } = render(<GlossaryItem term="acme" terms={[{ term, definition }]} />);
+
+  expect(container.querySelector('.glossary-item.highlight')).toHaveTextContent('acme');
+  expect(container.querySelector('.tooltip-content-body')).toHaveTextContent(`${term} - ${definition}`);
+});
+
+test('should output the term if the term does not exist', () => {
+  const term = 'something';
+  const { container } = render(<GlossaryItem term={term} terms={[]} />);
+
+  expect(container.querySelector('.glossary-item.highlight')).not.toBeInTheDocument();
+  expect(container.querySelector('span')).toHaveTextContent(term);
 });
