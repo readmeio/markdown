b4dd461e3dfeacd33abf1086a6863b9fa045b0b1
Kelly Joseph Price <kellyjosephprice@gmail.com>
2022-05-27 14:44:28 -0700

    test: update html contents + snapshot

--- __tests__/magic-block-parser.test.js
+++ __tests__/magic-block-parser.test.js
@@ -240,7 +240,7 @@ describe('Parse Magic Blocks', () => {
   it('Custom HTML Block', () => {
     const text = `[block:html]
     ${JSON.stringify({
-      html: '<h1>👋🌍</h1>\n<hr>\n<form>\n  <input name="test" value="hello" type="text"/>\n  <br/>\n  <a class="button">Go</a>\n</form>',
+      html: '<h1>👋🌍</h1>\n<hr>\n<a class="button">Go</a>',
     })}
     [/block]`;
 
