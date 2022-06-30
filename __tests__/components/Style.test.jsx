90218fa11fb679b1c365a5ea7d91a9563ea52537
Kelly Joseph Price <kellyjosephprice@gmail.com>
2022-06-30 16:02:35 -0700

    test: check for attrs

--- __tests__/components/Style.test.jsx
+++ __tests__/components/Style.test.jsx
@@ -6,6 +6,7 @@ describe('Style tag', () => {
   describe('safeMode = false', () => {
     it('renders as a style tag', () => {
       const md = '<style>{ background-color: salmon }</style>';
+
       expect(renderToString(react(md))).toMatchInlineSnapshot(
         `"<style data-reactroot=\\"\\">{ background-color: salmon }</style>"`
       );
@@ -15,6 +16,17 @@ describe('Style tag', () => {
   describe('safeMode = true', () => {
     it('renders the style in a `<pre>`', () => {
       const md = '<style>{ background-color: salmon }</style>';
+
+      expect(renderToString(react(md, { safeMode: true }))).toMatchInlineSnapshot(`
+        "<pre data-reactroot=\\"\\"><code>&lt;style&gt;
+        { background-color: salmon }
+        &lt;/style&gt;</code></pre>"
+      `);
+    });
+
+    it('renders the style in a `<pre>` when attrs are present', () => {
+      const md = '<style someAttr="muahaha">{ background-color: salmon }</style>';
+
       expect(renderToString(react(md, { safeMode: true }))).toMatchInlineSnapshot(`
         "<pre data-reactroot=\\"\\"><code>&lt;style&gt;
         { background-color: salmon }
