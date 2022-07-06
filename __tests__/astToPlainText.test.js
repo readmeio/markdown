278d5ed3e31b58799c46852d98c7d03e248df053
Kelly Joseph Price <kellyjosephprice@gmail.com>
2022-07-06 16:52:11 -0700

    feat: fix plain string compiler (#532)
    Fixes the `astToPlainText` compiler.
    
    I'm not sure how this working at all before, sometimes it returns a string, and sometimes it returns the passed in node?! Prior to this, it was calling `.runSync(ast)` which only [runs the `run` step](https://github.com/unifiedjs/unified#overview) and only calls `transformers` on the passed in `ast`. Theoretically, the correct call should be `.stringify(ast)`, which calls the `compile` step. 
    
    This PR reworks `astToPlainText()` to make the 'correct' call to `.stringify(ast)`. It also copies in the `hast-util-to-string` library so that it will use a space (` `) to join all the strings together.

---
+++ __tests__/astToPlainText.test.js
@@ -0,0 +1,67 @@
+import { hast, astToPlainText } from '../index';
+
+describe('astToPlainText()', () => {
+  it("converts br's to ''", () => {
+    const txt = `<br>`;
+
+    expect(astToPlainText(hast(txt))).toBe('');
+  });
+
+  it("converts hr's to ''", () => {
+    const txt = `<hr>`;
+
+    expect(astToPlainText(hast(txt))).toBe('');
+  });
+
+  it('converts flavored callouts', () => {
+    const txt = `
+> 📘 Title
+> 
+> Some body
+    `;
+
+    expect(astToPlainText(hast(txt))).toBe('Title Some body');
+  });
+
+  it('converts markdown tables', () => {
+    const txt = `
+| Header 1 | Header 2 |
+| :------- | :------- |
+| Cell 1   | Cell 2   |
+    `;
+
+    expect(astToPlainText(hast(txt))).toBe('Header 1 Header 2 Cell 1 Cell 2');
+  });
+
+  it('converts magic block tables', () => {
+    const txt = `
+[block:parameters]
+${JSON.stringify(
+  {
+    data: {
+      'h-0': 'Header 1',
+      'h-1': 'Header 2',
+      '0-0': 'Cell 1',
+      '0-1': 'Cell 2  \nCell 2.1',
+    },
+    cols: 2,
+    rows: 1,
+    align: ['left', 'left', 'left'],
+  },
+  null,
+  2
+)}
+[/block]
+    `;
+
+    expect(astToPlainText(hast(txt))).toBe('Header 1 Header 2 Cell 1 Cell 2  \nCell 2.1');
+  });
+
+  it('converts images', () => {
+    const txt = `
+![image **label**](http://placekitten.com/600/600)
+    `;
+
+    expect(astToPlainText(hast(txt))).toBe('');
+  });
+});
