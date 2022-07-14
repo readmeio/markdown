70c95de2ed475920d966ecbdf46a4b3205bdc98c
Jon Ursenbach <jon@ursenba.ch>
2022-07-14 14:02:51 -0700

    feat: updating our callout emoji regex to use `emoji-regex`

--- processor/parse/flavored/callout.js
+++ processor/parse/flavored/callout.js
@@ -1,9 +1,16 @@
-// @note: We'd like to allow any emoji to match, but included in the emoji
-// character set is [#*0-9].
-// https://www.unicode.org/Public/13.0.0/ucd/emoji/emoji-data.txt
-const rgx =
-  // eslint-disable-next-line unicorn/no-unsafe-regex
-  /^> ?(\u00a9|\u00ae|[\u2000-\u3300]|[\u{1f000}-\u{1fbff}])(?: {0,}(.+))?\n((?:>(?: .*)?\n)*)/u;
+const emojiRegex = require('emoji-regex');
+
+// We're pulling out the necessary regex from `emoji-regex` because it ships itself with regex
+// slashes and if we were to embed `emoji-regex` directly into our regex for matching callouts the
+// callout regex wouldn't work at all.
+let ergx = emojiRegex().toString();
+ergx = ergx.substring(1, ergx.length - 2);
+
+// `emoji-regex` can't match unicode emoji like `✎` that folks might use as callout icons so we
+// need to augment our regex a bit.
+const unicodeRgx = '\u00a9|\u00ae|[\u2000-\u3300]|[\u{1f000}-\u{1fbff}]';
+
+const rgx = new RegExp(`^> ?(${unicodeRgx}|${ergx})(?: {0,}(.+))?\n((?:>(?: .*)?\n)*)`, 'u');
 
 const themes = {
   '\uD83D\uDCD8': 'info',
@@ -31,7 +38,7 @@ function tokenizer(eat, value) {
   if (!rgx.test(value)) return true;
 
   // eslint-disable-next-line prefer-const
-  let [match, icon, title = '', text] = rgx.exec(value);
+  let [match, icon, title = '', text] = value.match(rgx);
 
   icon = icon.trim();
   text = text.replace(/^>(?:(\n)|(\s)?)/gm, '$1').trim();
