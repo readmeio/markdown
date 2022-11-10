395a67ef92be755811b54375d5b13987e7506648
Tony Li <runnabro@users.noreply.github.com>
2022-11-09 17:23:33 -0800

    feat(code): dark mode (#625)
    * feat(dark mode): add a way to test it
    
    * feat(code): dark mode
    
    * ehhh, it's always one behind?
    
    * fix(dark mode): remove unused hook
    
    * fix(code): wasnt updating on manual change
    
    * fix(code): add useMediaQuery hook
    
    * test: update snapshot :sadpanda:
    
    * fix: use platform for m1
    
    * test(code): mock matchmedia tests
    
    * test(code): fix tests
    
    * test(code): more fixed tests
    
    * fix(header): fix select colors
    
    * feat(usecolorscheme): some refactoring of dark mode
    
    * fix(dark mode): clean up some refactor code
    
    * test: snapshots
    
    * fix(dark mode): fix initial theme state
    
    * test: update snapshots + actually use installe chrome
    
    * fix(dark mode): more cleaning
    
    Co-authored-by: Kelly Joseph Price <kellyjosephprice@gmail.com>

--- __tests__/components/test.js
+++ __tests__/components/test.js
@@ -1,3 +1,6 @@
+import '../matchMedia.mock';
+
+// eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable no-eval */
 const { cleanup, fireEvent, render } = require('@testing-library/react');
 const React = require('react');
@@ -88,6 +91,7 @@ describe('Components', () => {
   it('Multi Code Block', () => {
     const tabs = '```\nhello\n```\n```php\nworld\n```\n\n';
     const rdmd = markdown.react(tabs);
+    document.querySelector('html').setAttribute('data-color-mode', 'auto');
     const { container } = render(rdmd);
 
     expect(container.querySelectorAll('pre')[1]).not.toHaveClass();
@@ -219,6 +223,7 @@ ${JSON.stringify({
   let container;
   beforeEach(() => {
     rdmd = markdown.react(tabs, { compatibilityMode: true });
+    document.querySelector('html').setAttribute('data-color-mode', 'auto');
     // eslint-disable-next-line testing-library/no-render-in-setup
     ({ container } = render(rdmd));
   });
