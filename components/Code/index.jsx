f6a3ca4b926150120999aaf568ff6fbcc4fec253
Tony Li <runnabro@users.noreply.github.com>
2022-11-17 12:51:28 -0800

    feat(code): remove dark mode (#637)
    * feat(code): remove dark mode
    
    * test: update visual regression snapshots
    
    Co-authored-by: Ellie Rossuck <erossuck@gmail.com>

--- components/Code/index.jsx
+++ components/Code/index.jsx
@@ -2,8 +2,6 @@ const copy = require('copy-to-clipboard');
 const PropTypes = require('prop-types');
 const React = require('react');
 
-const useColorScheme = require('../../hooks/useColorScheme');
-
 // Only load CodeMirror in the browser, for SSR
 // apps. Necessary because of people like this:
 // https://github.com/codemirror/CodeMirror/issues/3701#issuecomment-164904534
@@ -40,19 +38,17 @@ CopyCode.propTypes = {
 };
 
 function Code(props) {
-  const { children, className, copyButtons, lang, meta } = props;
+  const { children, className, copyButtons, lang, meta, theme } = props;
 
   const langClass = className.search(/lang(?:uage)?-\w+/) >= 0 ? className.match(/\s?lang(?:uage)?-(\w+)/)[1] : '';
   const language = canonicalLanguage(lang) || langClass;
 
   const codeRef = React.createRef();
 
-  const colorScheme = useColorScheme.default();
-
   const codeOpts = {
-    customTheme: colorScheme === 'light' ? 'neo' : 'material-palenight',
     inline: !lang,
     tokenizeVariables: true,
+    dark: theme === 'dark',
   };
 
   const codeContent =
@@ -63,7 +59,7 @@ function Code(props) {
       {copyButtons && <CopyCode className="fa" codeRef={codeRef} />}
       <code
         ref={codeRef}
-        className={['rdmd-code', `lang-${language}`, `theme-${colorScheme}`].join(' ')}
+        className={['rdmd-code', `lang-${language}`, `theme-${theme}`].join(' ')}
         data-lang={language}
         name={meta}
         suppressHydrationWarning={true}
