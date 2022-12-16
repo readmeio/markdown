8b35c40f704795bfa0fab3bb8ae6a080c2ee1ddd
Kelly Joseph Price <kellyjosephprice@gmail.com>
2022-12-16 14:17:55 -0800

    feat(code-tabs): treat single code blocks as tab

---
+++ __tests__/flavored-parsers/code-tabs.test.js
@@ -0,0 +1,43 @@
+import { mdast } from '../../index';
+
+describe('Code Tabs', () => {
+  it('can parse code tabs', () => {
+    const md = `
+\`\`\`
+First code block
+\`\`\`
+\`\`\`
+Second code block
+\`\`\`
+`;
+
+    expect(mdast(md)).toMatchSnapshot();
+  });
+
+  it('can parse lang and meta', () => {
+    const md = `
+\`\`\`javascript First Title
+First code block
+\`\`\`
+\`\`\`text
+Second code block
+\`\`\`
+`;
+    const ast = mdast(md);
+
+    expect(ast.children[0].children[0]).toStrictEqual(
+      expect.objectContaining({ lang: 'javascript', meta: 'First Title' })
+    );
+    expect(ast.children[0].children[1]).toStrictEqual(expect.objectContaining({ lang: 'text', meta: '' }));
+  });
+
+  it('treats a single code block with a lang as a code tab', () => {
+    const md = `
+\`\`\`text
+First code block
+\`\`\`
+`;
+
+    expect(mdast(md).children[0]).toHaveProperty('type', 'code-tabs');
+  });
+});
