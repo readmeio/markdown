5a005f54bede97e2312fd8c5260a8c49ff0ee0c5
Kelly Joseph Price <kellyjosephprice@gmail.com>
2022-12-20 14:39:04 -0800

    fix: single code tab title (#654)
    Oops, the previous PR wasn't setting the correct attributes.
    
    IMO though, those should be set in a hast transformer.

--- __tests__/transformers/single-code-tabs.test.js
+++ __tests__/transformers/single-code-tabs.test.js
@@ -1,9 +1,20 @@
 import { mdast } from '../../index';
 
 describe('Single Code Tab Transformer', () => {
-  it('wraps single code blocks with tabs if they have a lang or title set', () => {
+  it('wraps single code blocks with tabs if they have a lang set', () => {
     const md = `
 \`\`\`javascript
+const languageSet = true;
+\`\`\`
+`;
+
+    const tree = mdast(md);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('wraps single code blocks with tabs if they have a title set', () => {
+    const md = `
+\`\`\`javascript Testing
 const languageSet = true;
 \`\`\`
 `;
