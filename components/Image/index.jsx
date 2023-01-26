995c508800403f5b0e8b13f6d105f58efcdb2108
Rafe Goldberg <rafegoldberg@gmail.com>
2023-01-26 13:55:42 -0500

    style(img): pure css lightbox (#672)
    Co-authored-by: Kelly Joseph Price <kellyjosephprice@gmail.com>

--- components/Image/index.jsx
+++ components/Image/index.jsx
@@ -3,8 +3,6 @@
 const PropTypes = require('prop-types');
 const React = require('react');
 
-const Lightbox = require('./Lightbox');
-
 class Image extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +10,6 @@ class Image extends React.Component {
     this.state = {
       lightbox: false,
     };
-    this.lightbox = React.createRef();
 
     this.toggle = this.toggle.bind(this);
     this.handleKey = this.handleKey.bind(this);
@@ -20,26 +17,12 @@ class Image extends React.Component {
     this.isEmoji = props.className === 'emoji';
   }
 
-  componentDidMount() {
-    this.lightboxSetup();
-  }
-
   toggle(toState) {
     if (this.props.className === 'emoji') return;
 
     if (typeof toState === 'undefined') toState = !this.state.lightbox;
 
-    if (toState) this.lightboxSetup();
-
     this.setState({ lightbox: toState });
-  }
-
-  lightboxSetup() {
-    const $el = this.lightbox.current;
-    if ($el)
-      setTimeout(() => {
-        $el.scrollTop = ($el.scrollHeight - $el.offsetHeight) / 2;
-      }, 0);
   }
 
   handleKey(e) {
@@ -74,14 +57,15 @@ class Image extends React.Component {
     return (
       <span
         aria-label={alt}
-        className="img"
+        className={`img lightbox ${this.state.lightbox ? 'open' : 'closed'}`}
         onClick={() => this.toggle()}
         onKeyDown={this.handleKey}
         role={'button'}
         tabIndex={0}
       >
-        <img {...props} alt={alt} loading={lazy ? 'lazy' : ''} />
-        <Lightbox ref={this.lightbox} {...props} onScroll={() => this.toggle(false)} opened={this.state.lightbox} />
+        <span className="lightbox-inner">
+          <img {...props} alt={alt} loading={lazy ? 'lazy' : ''} />
+        </span>
       </span>
     );
   }
