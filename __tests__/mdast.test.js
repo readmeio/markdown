2d41714dddf4e3006862006b9b127f7fb679f137
Kelly Joseph Price <kellyjosephprice@gmail.com>
2023-08-08 13:21:45 -0700

    test: add more mdx tests

--- __tests__/mdast.test.js
+++ __tests__/mdast.test.js
@@ -7,4 +7,43 @@ describe('mdast(text, opts)', () => {
 
     expect(tree.children[0].type).toBe('jsx');
   });
+
+  it('should parse html as mdx', () => {
+    const mdx = `
+<h1>Heading</h1>
+`;
+    const tree = mdast(mdx, { mdx: true });
+
+    expect(tree.children[0].type).toBe('jsx');
+  });
+
+  it('should parse a magic block', () => {
+    const mdx = `
+[block:parameters]
+{
+  "data": {
+    "h-0": "new  \\nline",
+    "h-1": "",
+    "h-2": "",
+    "0-0": "",
+    "0-1": "",
+    "0-2": "",
+    "1-0": "",
+    "1-1": "",
+    "1-2": ""
+  },
+  "cols": 3,
+  "rows": 2,
+  "align": [
+    "left",
+    "left",
+    "left"
+  ]
+}
+[/block]
+    `;
+    const tree = mdast(mdx, { mdx: true });
+
+    expect(tree.children[0].type).toBe('table');
+  });
 });
