9e42dd9dedfee9da59ce7887dde947f940fa34f7
Daniels L <dannlee@gmail.com>
2023-08-15 09:19:08 -0700

    feat: adds new transformer for inline code nodes inside table cells (#805)
    This unescapes escaped pipe chars when rendering html. There appears to be a bug in remark or rehype where it displays the escaped pipe character in inline code spans within a table cell.
    
    When using pipe characters in code spans within tables, you have to escape the pipe character, otherwise, it would result in additional table cells.
    
    Consider the following markdown:
    
    ```
    | td |
    | -  |
    | `echo \| cat` |
    ```
    
    This would result in:
    
    ```
    <table>
      <thead>
        <tr>
          <td><code>echo \| cat</code></td>
        </tr>
      </thead>
    </table>
    ```
    
    Which doesn't seem like what was intended.
    
    Co-authored-by: Kelly Joseph Price <kellyjosephprice@gmail.com>

--- processor/transform/index.js
+++ processor/transform/index.js
@@ -1,1 +1,5 @@
-export { default as singleCodeTabs } from './single-code-tabs';
+import singleCodeTabs from './single-code-tabs';
+import tableCellInlineCode from './table-cell-inline-code';
+
+export const remarkTransformers = [singleCodeTabs];
+export const rehypeTransformers = [tableCellInlineCode];
