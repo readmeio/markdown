48b7ecdf4f662bf32ca4ef0c60ef3e50aa8f36a9
Kelly Joseph Price <kellyjosephprice@gmail.com>
2023-09-07 13:37:48 -0700

    fix: use slug instead of name (#815)
    Switching reusable contents attr from `name` to `slug` to match the backend.

--- __tests__/flavored-compilers/reusable-content.test.js
+++ __tests__/flavored-compilers/reusable-content.test.js
@@ -2,7 +2,7 @@ import { mdast, md } from '../../index';
 
 describe('reusable content compiler', () => {
   it('writes an undefined reusable content block back to markdown', () => {
-    const doc = '<RMReusableContent name="undefined" />';
+    const doc = '<RMReusableContent slug="undefined" />';
     const tree = mdast(doc);
 
     expect(md(tree)).toMatch(doc);
@@ -12,7 +12,7 @@ describe('reusable content compiler', () => {
     const reusableContent = {
       defined: '# Whoa',
     };
-    const doc = '<RMReusableContent name="defined" />';
+    const doc = '<RMReusableContent slug="defined" />';
     const tree = mdast(doc, { reusableContent });
 
     expect(tree.children[0].children[0].type).toBe('heading');
