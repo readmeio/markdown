4ecb97c0f9027bc3d4fba3472888a0a6ca45d0d9
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-03-07 12:38:21 -0800

    test: wtf

--- __tests__/browser/markdown.test.js
+++ __tests__/browser/markdown.test.js
@@ -31,6 +31,9 @@ describe('visual regression tests', () => {
       async doc => {
         const uri = `http://localhost:9966/?ci=true#${doc}`;
         await page.goto(uri, { waitUntil: 'networkidle0' });
+        await page.evaluate(() => {
+          window.scrollTo(0, window.document.body.scrollHeight);
+        });
         await sleep(500);
 
         const image = await page.screenshot({ fullPage: true });
