c9a1c1bc7b98e826b7532f53cea2b446354b0939
Trisha Le <trishaprilee@gmail.com>
2024-03-12 10:48:43 -0700

    refactor: reusable content parser (#838)
    * refactor: reusable content parser
    
    * chore: prettier
    
    * fix: export type
    
    * update regex
    
    Co-authored-by: Kelly Joseph Price <kellyjosephprice@gmail.com>
    
    ---------
    
    Co-authored-by: Kelly Joseph Price <kellyjosephprice@gmail.com>

---
+++ processor/parse/reusable-content-parser.js
@@ -0,0 +1,38 @@
+const { insertBlockTokenizerBefore } = require('./utils');
+
+export const type = 'reusable-content';
+
+function tokenizeReusableContent(eat, value, silent) {
+  const { tags, disabled, wrap = true } = this.data('reusableContent');
+  if (disabled) return false;
+
+  // Modifies the regular expression to match from
+  // the start of the line
+  const match = /^<(?<tag>[A-Z]\S+)\s*\/>\s*\n/.exec(value);
+
+  if (!match || !match.groups.tag) return false;
+  const { tag } = match.groups;
+
+  /* istanbul ignore if */
+  if (silent) return true;
+
+  const node = wrap
+    ? {
+        type: 'reusable-content',
+        tag,
+        children: tag in tags ? tags[tag] : [],
+      }
+    : tags[tag];
+
+  return eat(match[0])(node);
+}
+
+function parser() {
+  insertBlockTokenizerBefore.call(this, {
+    name: 'reusableContent',
+    before: 'html',
+    tokenizer: tokenizeReusableContent.bind(this),
+  });
+}
+
+export default parser;
--- processor/transform/index.js
+++ processor/transform/index.js
@@ -1,6 +1,5 @@
-import reusableContent from './reusable-content';
 import singleCodeTabs from './single-code-tabs';
 import tableCellInlineCode from './table-cell-inline-code';
 
-export const remarkTransformers = [singleCodeTabs, reusableContent];
+export const remarkTransformers = [singleCodeTabs];
 export const rehypeTransformers = [tableCellInlineCode];
