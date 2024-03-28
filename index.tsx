aee5bd6b8fce72160e4431c2d4d1edc4bb34acee
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-03-28 12:32:15 -0700

    feat: throw

--- index.tsx
+++ index.tsx
@@ -42,19 +42,14 @@ export const reactProcessor = (opts = {}) => {
 };
 
 export const react = (text: string, opts = {}) => {
-  try {
-    const code = compileSync(text, {
-      outputFormat: 'function-body',
-      providerImportSource: '@mdx-js/react',
-      remarkPlugins: [calloutTransformer],
-      ...opts,
-    });
+  const code = compileSync(text, {
+    outputFormat: 'function-body',
+    providerImportSource: '@mdx-js/react',
+    remarkPlugins: [calloutTransformer],
+    ...opts,
+  });
 
-    return code;
-  } catch (e) {
-    console.error(e);
-    return '';
-  }
+  return code;
 };
 
 export const run = (code: string, opts = {}) => {
