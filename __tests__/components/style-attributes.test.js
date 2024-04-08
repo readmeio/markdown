b6d34d01d9e08860fb79587ea11a000ac77e5a5d
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-04-08 16:18:46 -0700

    test: remove renderToSTring

--- __tests__/components/style-attributes.test.js
+++ __tests__/components/style-attributes.test.js
@@ -1,4 +1,4 @@
-const { renderToString } = require('react-dom/server');
+const { render } = require('@testing-library/react');
 
 const { react } = require('../../index');
 
@@ -6,20 +6,18 @@ describe('style attributes', () => {
   describe('safeMode = false', () => {
     it('renders style attributes', () => {
       const md = '<div style="background-color: salmon">fish content</div>';
+      const { container } = render(react(md));
 
-      expect(renderToString(react(md))).toMatchInlineSnapshot(
-        '"<div style=\\"background-color:salmon\\" data-reactroot=\\"\\">fish content</div>"',
-      );
+      expect(container.querySelector('[style]')).toBeVisible();
     });
   });
 
   describe('safeMode = true', () => {
     it('does not render style attributes', () => {
       const md = '<div style="background-color: salmon">fish content</div>';
+      const { container } = render(react(md, { safeMode: true }));
 
-      expect(renderToString(react(md, { safeMode: true }))).toMatchInlineSnapshot(
-        '"<div data-reactroot=\\"\\">fish content</div>"',
-      );
+      expect(container.querySelector('[style]')).toBeNull();
     });
   });
 });
