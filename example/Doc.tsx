ad077f5009c4a2ca772b9986eaf8d67f9ed7bf5d
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-04-23 15:03:14 -0700

    cleanup

--- example/Doc.tsx
+++ example/Doc.tsx
@@ -11,8 +11,6 @@ const Doc = () => {
   const lazyImages = searchParams.has('lazyImages');
   const safeMode = searchParams.has('safeMode');
 
-  console.log(docs, fixture);
-
   const [name, doc] =
     fixture === 'edited' ? [fixture, searchParams.get('edit') || ''] : [docs[fixture].name, docs[fixture].doc];
 
