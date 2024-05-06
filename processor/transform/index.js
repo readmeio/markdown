070dbde2e4397a30413e74f6b2dd66e4f7450cda
jennspencer <jennspencer@gmail.com>
2024-05-06 12:44:25 -0700

    add another export

--- processor/transform/index.js
+++ processor/transform/index.js
@@ -1,2 +1,3 @@
 export { default as singleCodeTabs } from './single-code-tabs';
 export { default as gemojiTransformer } from './gemoji+';
+export { default as calloutTransformer } from './callouts';
