db548457fa65013c9f6e4a1e9776fb50306bb59a
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-05-16 17:34:48 -0700

    chore: type readme-components

--- processor/transform/readme-components.ts
+++ processor/transform/readme-components.ts
@@ -1,5 +1,8 @@
-import { BlockContent, Paragraph, Root, TableRow } from 'mdast';
-import { MdxJsxFlowElement } from 'mdast-util-mdx';
+import { BlockContent, Code, Image, Paragraph, Parents, Table, TableRow } from 'mdast';
+import { Transform } from 'mdast-util-from-markdown';
+
+import { MdxJsxFlowElement, MdxJsxTextElement } from 'mdast-util-mdx';
+import { Callout, CodeTabs } from 'types';
 import { visit } from 'unist-util-visit';
 
 const types = {
@@ -12,88 +15,101 @@ const types = {
   td: 'tableCell',
 };
 
-const attributes = (jsx: MdxJsxFlowElement) =>
+const attributes = <T>(jsx: MdxJsxFlowElement | MdxJsxTextElement) =>
   jsx.attributes.reduce((memo, attr) => {
-    memo[attr.name] = attr.value;
+    if ('name' in attr) {
+      memo[attr.name] = attr.value;
+    }
+
     return memo;
-  }, {});
+  }, {} as T);
 
-const readmeComponents =
-  ({ components = {} } = {}) =>
-  (tree: Root) => {
-    visit(tree, ['mdxJsxFlowElement', 'mdxJsxTextElement'], (node, index, parent) => {
-      if (node.name in components) return;
+interface Options {
+  components: Record<string, string>;
+}
 
-      if (node.name === 'Code') {
-        const { position } = node;
-        const { value, lang = null, meta = null } = attributes(node);
+const coerceJsxToMd =
+  ({ components }) =>
+  (node: MdxJsxFlowElement | MdxJsxTextElement, index: number, parent: Parents) => {
+    if (node.name in components) return;
 
-        const mdNode = {
-          lang,
-          meta,
-          position,
-          type: 'code',
-          value,
-          data: {
-            hProperties: { value, lang, meta },
-          },
-        };
+    if (node.name === 'Code') {
+      const { position } = node;
+      const { value, lang = null, meta = null } = attributes<Pick<Code, 'value' | 'lang' | 'meta'>>(node);
 
-        parent.children[index] = mdNode;
-      } else if (node.name === 'Image') {
-        const { position } = node;
-        const { alt = '', src, title = null } = attributes(node);
+      const mdNode: Code = {
+        lang,
+        meta,
+        position,
+        type: 'code',
+        value,
+        data: {
+          hProperties: { value, lang, meta },
+        },
+      };
 
-        const mdNode = {
-          alt,
-          position,
-          title,
-          type: 'image',
-          url: src,
-        };
+      parent.children[index] = mdNode;
+    } else if (node.name === 'Image') {
+      const { position } = node;
+      const { alt = '', url, title = null } = attributes<Pick<Image, 'alt' | 'url' | 'title'>>(node);
 
-        parent.children[index] = mdNode;
-      } else if (node.name === 'Table') {
-        const { children, position } = node;
-        const { align = [...new Array(node.children.length)].map(() => null) } = attributes(node);
+      const mdNode: Image = {
+        alt,
+        position,
+        title,
+        type: 'image',
+        url,
+      };
 
-        const mdNode = {
-          align,
-          type: 'table',
-          position,
-          children,
-        };
+      parent.children[index] = mdNode;
+    } else if (node.name === 'Table') {
+      const { children, position } = node;
+      const { align = [...new Array(node.children.length)].map(() => null) } = attributes<Pick<Table, 'align'>>(node);
 
-        parent.children[index] = mdNode;
-      } else if (node.name in types) {
-        const hProperties = attributes(node);
+      const mdNode: Table = {
+        align,
+        type: 'table',
+        position,
+        // @ts-ignore
+        children,
+      };
 
-        const mdNode = {
-          children: node.children,
-          type: types[node.name],
-          ...(['tr', 'td'].includes(node.name)
-            ? {}
-            : {
-                data: {
-                  hName: node.name,
-                  ...(Object.keys(hProperties).length ? { hProperties } : {}),
-                },
-              }),
-          position: node.position,
-        };
-
-        parent.children[index] = mdNode;
-      }
-    });
-
-    visit(tree, 'paragraph', (node: Paragraph, index: number, parent: BlockContent | TableRow) => {
-      if (parent.type !== 'tableRow') return;
+      parent.children[index] = mdNode;
+    } else if (node.name in types) {
+      const hProperties = attributes(node);
 
       // @ts-ignore
-      parent.children.splice(index, 1, ...node.children);
-    });
+      const mdNode: Callout | CodeTabs = {
+        children: node.children as any,
+        type: types[node.name],
+        ...(['tr', 'td'].includes(node.name)
+          ? {}
+          : {
+              data: {
+                hName: node.name,
+                ...(Object.keys(hProperties).length ? { hProperties } : {}),
+              },
+            }),
+        position: node.position,
+      };
 
-    return tree;
+      parent.children[index] = mdNode;
+    }
   };
+
+const readmeComponents = (opts: Options) => (): Transform => tree => {
+  visit(tree, 'mdxJsxFlowElement', coerceJsxToMd(opts));
+  visit(tree, 'mdxJsxTextElement', coerceJsxToMd(opts));
+
+  visit(tree, 'paragraph', (node, index, parent) => {
+    // @ts-ignore
+    if (parent.type !== 'tableRow') return;
+
+    // @ts-ignore
+    parent.children.splice(index, 1, ...node.children);
+  });
+
+  return tree;
+};
 
 export default readmeComponents;
