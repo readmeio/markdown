6caaafd19de9c13ea4f48df23f74d889725b77ba
jennspencer <jennspencer@gmail.com>
2024-05-23 11:08:35 -0700

    fix: compilers (#882)
    [![PR App][icn]][demo] | Fix RM-9795 RM-9792
    :-------------------:|:----------:
    
    ## 🧰 Changes
    
    needed to adjust the output for the image & embed & html block compilers
    
    ## 🧬 QA & Testing
    
    - [Broken on production][prod].
    - [Working in this PR app][demo].
    
    
    [demo]: https://markdown-pr-PR_NUMBER.herokuapp.com
    [prod]: https://SUBDOMAIN.readme.io
    [icn]:
    https://user-images.githubusercontent.com/886627/160426047-1bee9488-305a-4145-bb2b-09d8b757d38a.svg

--- processor/compile/embed.ts
+++ processor/compile/embed.ts
@@ -3,7 +3,12 @@ import type { Embed } from "types";
 const embed = (node: Embed) => {
   const { image, favicon, iframe, title, url } = node.data?.hProperties || {};
   const complexEmbed: boolean = Boolean(image) || Boolean(favicon) || iframe;
-  if (complexEmbed) return `<Embed ${{...node.data?.hProperties}} />`;
+
+  if (complexEmbed) {
+    const attributes = Object.keys(node.data?.hProperties).map(key => `${key}="${node.data?.hProperties[key]}"`).join(' ')
+    // TODO: make this a util
+    return `<Embed ${attributes} />`;
+  }
 
   return `[${title}](${url} "@embed")'`;
 }
--- processor/compile/image.ts
+++ processor/compile/image.ts
@@ -3,7 +3,11 @@ import type { Image } from "mdast";
 const image = (node: Image) => {
   const { align, className, width } = node.data?.hProperties || {};
   const complexImage: boolean = Boolean(width) || Boolean(className) || Boolean(align);
-  if (complexImage) return `<Image ${{...node.data?.hProperties}} />`;
+  
+  if (complexImage) {
+    const attributes = Object.keys(node.data?.hProperties).map(key => `${key}="${node.data?.hProperties[key]}"`).join(' ')
+    return `<Image ${attributes} />`;
+  }
 
   return `![${node.alt}](${node.url}${node.title ? ` "${node.title}")` : ')'}`;
 }
