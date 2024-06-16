5a543fc0dd9ef12b2a65c6b8ed28685c4d461298
jennspencer <jennspencer@gmail.com>
2024-06-16 12:32:02 -0700

    html block compiler tweaks

--- processor/compile/html-block.ts
+++ processor/compile/html-block.ts
@@ -4,7 +4,7 @@ import { HTMLBlock } from '../../types';
 const htmlBlock = (node: HTMLBlock) => {
   const { runScripts, html } = node.data.hProperties;
 
-  return `<HTMLBlock${runScripts != null ? ' runScripts="' + runScripts + '"' : ''}>{${ html }}</HTMLBlock>`;
+  return `<HTMLBlock${runScripts != null ? ' runScripts="' + runScripts + '"' : ''}>{\`${ html }\`}</HTMLBlock>`;
 }
 
 export default htmlBlock;
