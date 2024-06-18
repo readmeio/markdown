cd82b31f5ef76845e5718d6b53a7fcbe6b64ca1b
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-06-18 10:48:42 -0700

    tests: tests

---
+++ __tests__/compilers/html-block.test.ts
@@ -0,0 +1,33 @@
+import { mdast, mdx } from '../../index';
+
+describe('html-block compiler', () => {
+  it('compiles html blocks within containers', () => {
+    const markdown = `
+> 🚧 It compiles!
+>
+> <HTMLBlock>{\`
+>   <strong style="color: olive">Hello, World!</strong>
+> \`}</HTMLBlock>
+`;
+
+    expect(mdx(mdast(markdown)).trim()).toBe(markdown.trim());
+  });
+
+  it('compiles html blocks with indents', () => {
+    const markdown = `
+<HTMLBlock>{\`
+  <pre><code>
+const foo = () => {
+  const bar = {
+    baz: 'blammo'
+  }
+
+  return bar
+}
+  </code></pre>
+\`}</HTMLBlock>
+`;
+
+    expect(mdx(mdast(markdown)).trim()).toBe(markdown.trim());
+  });
+});
