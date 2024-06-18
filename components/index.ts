a9df53ea74c9e4194a4a184774e3867673449fb6
Trisha Le <trishaprilee@gmail.com>
2024-06-18 16:49:24 -0700

    order

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 export { default as Anchor } from './Anchor';
 export { default as Callout } from './Callout';
+export { default as Cards } from './CardsGrid';
 export { default as Code } from './Code';
 export { default as CodeTabs } from './CodeTabs';
 export { default as Embed } from './Embed';
@@ -8,5 +9,4 @@ export { default as HTMLBlock } from './HTMLBlock';
 export { default as Heading } from './Heading';
 export { default as Image } from './Image';
 export { default as Table } from './Table';
-export { default as Cards } from './CardsGrid';
 export { default as TableOfContents } from './TableOfContents';
