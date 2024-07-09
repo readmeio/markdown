df5d859824574a4c9a06fbee0365f5bb9b97b189
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-07-09 16:37:10 -0700

    test: failing case

--- __tests__/lib/hast.test.ts
+++ __tests__/lib/hast.test.ts
@@ -21,6 +21,84 @@ describe('hast transformer', () => {
 
     expect(hast(md, { components })).toStrictEqualExceptPosition(expected);
   });
+
+  it.only('parses html tags', () => {
+    const md = '<h2>Nice</h2>';
+    const tree = hast(md);
+    expect(tree).toMatchInlineSnapshot(`
+      {
+        "children": [
+          {
+            "children": [
+              {
+                "children": [
+                  {
+                    "position": {
+                      "end": {
+                        "column": 9,
+                        "line": 1,
+                        "offset": 8,
+                      },
+                      "start": {
+                        "column": 5,
+                        "line": 1,
+                        "offset": 4,
+                      },
+                    },
+                    "type": "text",
+                    "value": "Nice",
+                  },
+                ],
+                "position": {
+                  "end": {
+                    "column": 14,
+                    "line": 1,
+                    "offset": 13,
+                  },
+                  "start": {
+                    "column": 1,
+                    "line": 1,
+                    "offset": 0,
+                  },
+                },
+                "properties": {},
+                "tagName": "div",
+                "type": "element",
+              },
+            ],
+            "position": {
+              "end": {
+                "column": 14,
+                "line": 1,
+                "offset": 13,
+              },
+              "start": {
+                "column": 1,
+                "line": 1,
+                "offset": 0,
+              },
+            },
+            "properties": {},
+            "tagName": "p",
+            "type": "element",
+          },
+        ],
+        "position": {
+          "end": {
+            "column": 14,
+            "line": 1,
+            "offset": 13,
+          },
+          "start": {
+            "column": 1,
+            "line": 1,
+            "offset": 0,
+          },
+        },
+        "type": "root",
+      }
+    `);
+  });
 });
 
 describe('hastFromHtml', () => {
