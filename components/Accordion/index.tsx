1c01885fc3e7f4193708bfd2cf6983b8485f25e3
Trisha Le <trishaprilee@gmail.com>
2024-07-25 11:21:07 -0700

    icon

--- components/Accordion/index.tsx
+++ components/Accordion/index.tsx
@@ -3,19 +3,19 @@ import React, { useState } from 'react';
 import './style.scss';
 
 const Accordion = ({ children, title }) => {
-  const [active, setActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const handleToggle = () => {
-    setActive(!active);
+    setIsActive(!isActive);
   }
   
   return (
     <div className="Accordion">
       <button className="title" onClick={handleToggle}>
         {title}
-        <i className={`icon${active && '_active'} fa fa-chevron-right`}></i>
+        <i className={`icon${isActive ? '_active' : ''} fa fa-chevron-right`}></i>
       </button>
-      <div className={`content content${active && '_active'}`}>{children}</div>
+      <div className={`content content${isActive ? '_active' : ''}`}>{children}</div>
     </div>
   );
 };
