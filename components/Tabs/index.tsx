f13eb3fcbfd0e48a49d954b9fcd52539f3ea1df7
Trisha Le <trishaprilee@gmail.com>
2024-08-06 15:18:01 -0700

    feat: tabs component

---
+++ components/Tabs/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import './style.scss';
+
+export const Tab = ({ children }) => {
+  return (
+    <div className="TabContent">
+      {children}
+    </div>
+  )
+}
+
+const Tabs = ({ children }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div className="TabGroup">
+      <header>
+        <nav>
+          {children.map((tab, index: number) => 
+            <button className={`tab${activeTab === index ? '_active' : ''}`} key={tab.props.title} onClick={() => setActiveTab(index)}>
+              {tab.props.title}
+            </button>
+          )}
+        </nav>
+      </header>
+      <section>
+        {children[activeTab]}
+      </section>
+    </div>
+  );
+};
+
+export default Tabs;
