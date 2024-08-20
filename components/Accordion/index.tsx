742d96d5e3229bed9c9e0f9b8296df0e9b4d9587
Trisha Le <trishaprilee@gmail.com>
2024-08-20 13:44:28 -0700

    ty rafe

--- components/Accordion/index.tsx
+++ components/Accordion/index.tsx
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 
 import './style.scss';
 
-const Accordion = ({ children, icon, title }) => {
+const AccordionTest = ({ children, icon, iconColor, title }) => {
+  return (
+    <details className="Accordion">
+      <summary className="Accordion-title">
+        <i className={`Accordion-icon fa ${icon}`} style={{ color: `${iconColor}` }}></i>{title}
+      </summary>
+      <div className="Accordion-content">{children}</div>
+    </details>
+  );
+};
+
+const Accordion = ({ children, icon, iconColor, title }) => {
   const [isActive, setIsActive] = useState(false);
 
   const handleToggle = () => {
@@ -13,11 +24,11 @@ const Accordion = ({ children, icon, title }) => {
     <div className="Accordion">
       <button className="Accordion-title" onClick={handleToggle}>
         <i className={`Accordion-arrow${isActive ? '_active' : ''} fa fa-chevron-right`}></i>
-        <div><i className={`Accordion-icon fa ${icon}`}></i>{title}</div>
+        <div><i className={`Accordion-icon fa ${icon}`} style={{ color: `${iconColor}` }}></i>{title}</div>
       </button>
       <div className={`Accordion-content${isActive ? '_active' : ''}`}>{children}</div>
     </div>
   );
 };
 
-export default Accordion;
+export default AccordionTest;
--- components/Cards/index.tsx
+++ components/Cards/index.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import './style.scss';
 
-export const Card = ({ children, icon, iconColor, title }) => {
+export const Card = ({ children, href, icon, iconColor, target, title }) => {
+  const Tag = href ? 'a' : 'div';
   return (
-    <div className="Card">
+    <Tag className="Card" href={href} target={target}>
       {icon && <i className={`Card-icon fa ${icon}`} style={{ color: `${iconColor}` }}></i>}
       {title && <h3 className='Card-title'>{title}</h3>}
       <div className="Card-content">{children}</div>
-    </div>
+    </Tag>
   )
 }
 
