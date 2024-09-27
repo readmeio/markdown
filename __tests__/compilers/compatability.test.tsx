15c3329dd59e91069137b63e45b0b35daba5c8d8
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-09-27 13:05:08 -0700

    fix: tables with inline code with newlines

--- __tests__/compilers/compatability.test.tsx
+++ __tests__/compilers/compatability.test.tsx
@@ -409,4 +409,59 @@ ${JSON.stringify(
       "
     `);
   });
+
+  it.only('compiles tables with inline code with newlines', () => {
+    const md = `
+[block:parameters]
+${JSON.stringify(
+  {
+    data: {
+      'h-0': 'Field',
+      'h-1': 'Description',
+      '0-0': 'orderby',
+      '0-1': '`{\n "field": "ID",\n "type": "ASC"\n }`',
+    },
+    cols: 2,
+    rows: 1,
+    align: ['left', 'left'],
+  },
+  null,
+  2,
+)}
+[/block]
+    `;
+
+    const rmdx = mdx(rdmd.mdast(md));
+    expect(rmdx).toMatchInlineSnapshot(`
+      "<Table align={["left","left"]}>
+        <thead>
+          <tr>
+            <th style={{ textAlign: "left" }}>
+              Field
+            </th>
+
+            <th style={{ textAlign: "left" }}>
+              Description
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td style={{ textAlign: "left" }}>
+              orderby
+            </td>
+
+            <td style={{ textAlign: "left" }}>
+              \`{
+               "field": "ID",
+               "type": "ASC"
+               }\`
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      "
+    `);
+  });
 });
