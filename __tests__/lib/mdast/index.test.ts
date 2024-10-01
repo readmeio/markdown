3faa95d2da4f63fa129a604ad93547ce973792ae
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-10-01 15:22:47 -0700

    fix: inline mdx images (#983)
    [![PR App][icn]][demo] |
    :-------------------:|
    
    ## 🧰 Changes
    
    Fixes loading inline images.
    
    The editor was throwing away inlined MDX images! This PR converts them
    to plain inline markdown images.
    
    ## 🧬 QA & Testing
    
    - [Broken on production][prod].
    - [Working in this PR app][demo].
    
    
    [demo]: https://markdown-pr-PR_NUMBER.herokuapp.com
    [prod]: https://SUBDOMAIN.readme.io
    [icn]:
    https://user-images.githubusercontent.com/886627/160426047-1bee9488-305a-4145-bb2b-09d8b757d38a.svg

--- __tests__/lib/mdast/index.test.ts
+++ __tests__/lib/mdast/index.test.ts
@@ -1,21 +1,35 @@
 import { mdast } from '../../../lib';
-import { readdir, readFile } from 'node:fs/promises';
+
+import nullAttributesMdx from './null-attributes/in.mdx?raw';
+import nullAttributesJson from './null-attributes/out.json';
+
+import tablesMdx from './tables/in.mdx?raw';
+import tablesJson from './tables/out.json';
+
+import variablesMdx from './variables/in.mdx?raw';
+import variablesJson from './variables/out.json';
+
+import inlineImagesMdx from './images/inline/in.mdx?raw';
+import inlineImagesJson from './images/inline/out.json';
 
 describe('mdast transformer', async () => {
-  const cases = await Promise.all(
-    (await readdir(__dirname))
-      .filter(name => !name.match(/.[tj]sx?/))
-      .map(async dirname => {
-        return {
-          name: dirname,
-          mdx: await readFile(`${__dirname}/${dirname}/in.mdx`, { encoding: 'utf8' }),
-          ast: JSON.parse(await readFile(`${__dirname}/${dirname}/out.json`, { encoding: 'utf8' })),
-        };
-      }),
-  );
-
-  it.each(cases)('parses $name', ({ mdx, ast }) => {
+  it('parses null attributes', () => {
     // @ts-ignore
-    expect(mdast(mdx)).toStrictEqualExceptPosition(ast);
+    expect(mdast(nullAttributesMdx)).toStrictEqualExceptPosition(nullAttributesJson);
+  });
+
+  it('parses tables', () => {
+    // @ts-ignore
+    expect(mdast(tablesMdx)).toStrictEqualExceptPosition(tablesJson);
+  });
+
+  it('parses variables', () => {
+    // @ts-ignore
+    expect(mdast(variablesMdx)).toStrictEqualExceptPosition(variablesJson);
+  });
+
+  it('parses inline images', () => {
+    // @ts-ignore
+    expect(mdast(inlineImagesMdx)).toStrictEqualExceptPosition(inlineImagesJson);
   });
 });
