31d14c454422641d746e77b20397db13829763ab
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-10-08 11:49:19 -0700

    fix: migrate table newlines (#12959)
    | 📖 [PR App][pr] | 🎫 Resolve RM-11018, RM-11002, CX-1116, RM-10942 |
    | :-------------: | :-----------------: |
    
    ## 🧰 Changes
    
    Fixes migrating tables with newlines.
    
    1. Convert inline code spans with newlines into code blocks
    2. Wrap content in a paragraph when needed to avoid excessive newlines
    3. Attempt to fix malformed lists in tables created by our editor
    
    ## 💁 Customer Impact
    
    <!-- PLEASE DESCRIBE IF THIS CHANGE AFFECTS CUSTOMERS AND HOW -->
    <!-- This is extremely useful for Tony and Marc writing the changelog!
    -->
    
    ## 🧬 QA & Testing
    
    - [Broken on next][next].
    - [Working in this PR][pr]!
    
    [next]: https://next.readme.ninja
    [pr]: https://readme-pr-12959.readme.ninja
    [ui]: https://readme-pr-12959.readme.ninja/ui
    
    <!-- Uncomment and unescape this if you don't want a PR app! -->
    <!-- \[skip preview\] -->

---
+++ tables.test.ts
@@ -0,0 +1,218 @@
+import * as rmdx from '@readme/mdx';
+
+import { rdmdCompatParser as mdast } from '@readme/backend/models/project/lib/migrateMdx/rdmdCompatParser';
+
+describe('mdx migration of tables', () => {
+  it('compiles tables with newlines and inline code', () => {
+    const md = `
+[block:parameters]
+${JSON.stringify(
+  {
+    data: {
+      'h-0': 'Field',
+      'h-1': 'Description',
+      '0-0': 'orderby',
+      '0-1': '`{\n  "field": "ID",\n  "type": "ASC"\n}`',
+    },
+    cols: 2,
+    rows: 1,
+    align: ['left', 'left'],
+  },
+  null,
+  2,
+)}
+[/block]
+    `;
+
+    const ast = mdast(md);
+    const mdx = rmdx.mdx(ast);
+    expect(mdx).toMatchInlineSnapshot(`
+      "<Table align={["left","left"]}>
+        <thead>
+          <tr>
+            <th style={{ textAlign: "left" }}>
+              Field
+            </th>
+
+            <th style={{ textAlign: "left" }}>
+              Description
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td style={{ textAlign: "left" }}>
+              orderby
+            </td>
+
+            <td style={{ textAlign: "left" }}>
+              \`\`\`
+              {
+                "field": "ID",
+                "type": "ASC"
+              }
+              \`\`\`
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      "
+    `);
+  });
+
+  it('compiles tables with pseudo-lists', () => {
+    const md = `
+[block:parameters]
+${JSON.stringify(
+  {
+    data: {
+      'h-0': 'Field',
+      'h-1': 'Description',
+      '0-0': 'numbered lists',
+      '0-1': '1. numbered lists\n2. are supported too',
+      '1-0': 'loose lists',
+      '1-1': '- loose lists\n\n- are supported too',
+    },
+    cols: 2,
+    rows: 2,
+    align: ['left', 'left'],
+  },
+  null,
+  2,
+)}
+[/block]
+`;
+    const mdx = rmdx.mdx(mdast(md));
+
+    expect(mdx).toMatchInlineSnapshot(`
+      "<Table align={["left","left"]}>
+        <thead>
+          <tr>
+            <th style={{ textAlign: "left" }}>
+              Field
+            </th>
+
+            <th style={{ textAlign: "left" }}>
+              Description
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td style={{ textAlign: "left" }}>
+              numbered lists
+            </td>
+
+            <td style={{ textAlign: "left" }}>
+              1. numbered lists
+              2. are supported too
+            </td>
+          </tr>
+
+          <tr>
+            <td style={{ textAlign: "left" }}>
+              loose lists
+            </td>
+
+            <td style={{ textAlign: "left" }}>
+              * loose lists
+
+              * are supported too
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      "
+    `);
+  });
+
+  it('compiles tables with broken pseudo-lists', () => {
+    const md = `
+[block:parameters]
+${JSON.stringify(
+  {
+    data: {
+      'h-0': 'Field',
+      'h-1': 'Description',
+      '0-0': 'reproduction',
+      '0-1': 'Oh no\n\n_no no no\n_no no no\n*Im so sorry',
+      '1-0': 'reproduction 2',
+      '1-1': 'Oh no\n\n\\_no no no\n\\_no no no\n\\*Im so sorry',
+      '2-0': 'reproduction 3',
+      '2-1': 'Oh no\n\n  \\_  no no no\n  _ no no no\n  \\*Im so sorry',
+    },
+    cols: 2,
+    rows: 3,
+    align: ['left', 'left'],
+  },
+  null,
+  2,
+)}
+[/block]
+`;
+    const mdx = rmdx.mdx(mdast(md));
+
+    expect(mdx).toMatchInlineSnapshot(`
+      "<Table align={["left","left"]}>
+        <thead>
+          <tr>
+            <th style={{ textAlign: "left" }}>
+              Field
+            </th>
+
+            <th style={{ textAlign: "left" }}>
+              Description
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td style={{ textAlign: "left" }}>
+              reproduction
+            </td>
+
+            <td style={{ textAlign: "left" }}>
+              Oh no
+
+              * no no no
+              * no no no
+              * Im so sorry
+            </td>
+          </tr>
+
+          <tr>
+            <td style={{ textAlign: "left" }}>
+              reproduction 2
+            </td>
+
+            <td style={{ textAlign: "left" }}>
+              Oh no
+
+              * no no no
+              * no no no
+              * Im so sorry
+            </td>
+          </tr>
+
+          <tr>
+            <td style={{ textAlign: "left" }}>
+              reproduction 3
+            </td>
+
+            <td style={{ textAlign: "left" }}>
+              Oh no
+
+              * no no no
+              * no no no
+              * Im so sorry
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      "
+    `);
+  });
+});
