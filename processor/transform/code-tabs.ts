59b656071007c86b7516fec78e123c2f7e01038a
Rafe Goldberg <rafegoldberg@gmail.com>
2024-10-09 07:28:02 -0400

    test: fix failing expectations

--- processor/transform/code-tabs.ts
+++ processor/transform/code-tabs.ts
@@ -6,54 +6,54 @@ import { NodeTypes } from '../../enums';
 const isCode = (node: Node): node is Code => node?.type === 'code';
 
 const codeTabsTransformer =
-  ({ copyButtons }: { copyButtons?: boolean }) =>
+  ({ copyButtons }: { copyButtons?: boolean } = {}) =>
   (tree: Node) => {
-  visit(tree, 'code', (node: Code) => {
-    const { lang, meta, value } = node;
-    node.data = {
+    visit(tree, 'code', (node: Code) => {
+      const { lang, meta, value } = node;
+      node.data = {
         hProperties: { lang, meta, value, copyButtons },
-    };
-  });
+      };
+    });
 
-  visit(tree, 'code', (node: Code, index: number, parent: BlockContent) => {
-    if (parent.type === 'code-tabs' || !('children' in parent)) return;
+    visit(tree, 'code', (node: Code, index: number, parent: BlockContent) => {
+      if (parent.type === 'code-tabs' || !('children' in parent)) return;
 
-    const length = parent.children.length;
-    let children = [node];
-    let walker = index + 1;
+      const length = parent.children.length;
+      let children = [node];
+      let walker = index + 1;
 
-    while (walker <= length) {
-      const sibling = parent.children[walker];
-      if (!isCode(sibling)) break;
+      while (walker <= length) {
+        const sibling = parent.children[walker];
+        if (!isCode(sibling)) break;
 
-      const olderSibling = parent.children[walker - 1];
-      if (olderSibling.position.end.offset + sibling.position.start.column !== sibling.position.start.offset) break;
+        const olderSibling = parent.children[walker - 1];
+        if (olderSibling.position.end.offset + sibling.position.start.column !== sibling.position.start.offset) break;
 
-      children.push(sibling);
-      walker++;
-    }
+        children.push(sibling);
+        walker++;
+      }
 
-    // If there is a single code block, and it has either a title or a
-    // language set, let's display it by wrapping it in a code tabs block.
-    // Othewise, we can leave early!
-    if (children.length === 1 && !(node.lang || node.meta)) return;
+      // If there is a single code block, and it has either a title or a
+      // language set, let's display it by wrapping it in a code tabs block.
+      // Othewise, we can leave early!
+      if (children.length === 1 && !(node.lang || node.meta)) return;
 
-    const codeTabs: CodeTabs = {
-      type: NodeTypes.codeTabs,
-      children,
-      data: {
-        hName: 'CodeTabs',
-      },
-      position: {
-        start: children[0].position.start,
-        end: children[children.length - 1].position.end,
-      },
-    };
+      const codeTabs: CodeTabs = {
+        type: NodeTypes.codeTabs,
+        children,
+        data: {
+          hName: 'CodeTabs',
+        },
+        position: {
+          start: children[0].position.start,
+          end: children[children.length - 1].position.end,
+        },
+      };
 
-    parent.children.splice(index, children.length, codeTabs);
-  });
+      parent.children.splice(index, children.length, codeTabs);
+    });
 
-  return tree;
-};
+    return tree;
+  };
 
 export default codeTabsTransformer;
