98b1b6c85f504210f501ff3bbb295c790790b3d2
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-10-30 11:58:00 -0700

    tests: tests

--- __tests__/migration/emphasis.test.ts
+++ __tests__/migration/emphasis.test.ts
@@ -1,10 +1,10 @@
-import * as rmdx from '../../index';
+import { migrate } from '../../index';
 
 describe('migrating emphasis', () => {
   it('trims whitespace surrounding phrasing content (emphasis, strong, etc)', () => {
     const md = '** bold ** and _ italic _ and ***   bold italic ***';
 
-    const mdx = rmdx.mdx(rmdx.mdastV6(md));
+    const mdx = migrate(md);
     expect(mdx).toMatchInlineSnapshot(`
       "**bold** and *italic* and ***bold italic***
       "
@@ -14,7 +14,7 @@ describe('migrating emphasis', () => {
   it('moves whitespace surrounding phrasing content (emphasis, strong, etc) to the appropriate place', () => {
     const md = '**bold **and also_ italic_ and*** bold italic***aaaaaah';
 
-    const mdx = rmdx.mdx(rmdx.mdastV6(md));
+    const mdx = migrate(md);
     expect(mdx).toMatchInlineSnapshot(`
       "**bold** and also *italic* and ***bold italic***aaaaaah
       "
@@ -25,9 +25,29 @@ describe('migrating emphasis', () => {
     const md =
       '*the recommended initial action is to**initiate a[reversal operation (rollback)](https://docs.jupico.com/reference/ccrollback)**. *';
 
-    const mdx = rmdx.mdx(rmdx.mdastV6(md));
+    const mdx = migrate(md);
     expect(mdx).toMatchInlineSnapshot(`
       "*the recommended initial action is to**initiate a[reversal operation (rollback)](https://docs.jupico.com/reference/ccrollback)**.*
+      "
+    `);
+  });
+
+  it('removes unneccesary escapes from emphasis', () => {
+    const md = `[FOO_BAR] and [FOO__BAR]`;
+    const mdx = migrate(md);
+
+    expect(mdx).toMatchInlineSnapshot(`
+      "[FOO_BAR][FOO_BAR] and [FOO__BAR][FOO__BAR]
+      "
+    `);
+  });
+
+  it('migrates deprecated internal emphasis', () => {
+    const md = `Foo__Bar__Baz`;
+    const mdx = migrate(md);
+
+    expect(mdx).toMatchInlineSnapshot(`
+      "Foo**Bar**Baz
       "
     `);
   });
