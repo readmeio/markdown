fc2f069626a087b56ac9e35f7991280e3db7682b
Kelly Joseph Price <kellyjosephprice@gmail.com>
2024-10-30 10:56:51 -0700

    feat: migrate export + html spaces

--- index.tsx
+++ index.tsx
@@ -13,5 +13,5 @@ const utils = {
   calloutIcons: {},
 };
 
-export { compile, hast, run, mdast, mdastV6, mdx, plain, remarkPlugins, tags } from './lib';
+export { compile, hast, run, mdast, mdastV6, mdx, migrate, plain, remarkPlugins, tags } from './lib';
 export { Components, utils };
