afe207caed72b94041ba3256ef8e9ee2e75f4542
davinhazard <davinhazard@gmail.com>
2025-01-23 16:48:47 -0500

    add missing export

--- index.tsx
+++ index.tsx
@@ -13,5 +13,5 @@ const utils = {
   calloutIcons: {},
 };
 
-export { compile, hast, run, mdast, mdastV6, mdx, migrate, plain, remarkPlugins, tags } from './lib';
+export { compile, exports, hast, run, mdast, mdastV6, mdx, migrate, plain, remarkPlugins, tags } from './lib';
 export { Components, utils };
