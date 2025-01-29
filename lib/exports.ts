f6e7b5ffa7157427c194a541202e16e451fe53c0
davinhazard <davinhazard@gmail.com>
2025-01-29 11:38:12 -0500

    refactor exports util and update test cases

--- lib/exports.ts
+++ lib/exports.ts
@@ -3,19 +3,77 @@ import mdast from './mdast';
 import { isMDXEsm } from '../processor/utils';
 import { MdxjsEsm } from 'mdast-util-mdx';
 
-const EXPORT_NAME_REGEX = /export\s+(?:const|let|var|function)\s+(\w+)/;
+
 
 const exports = (doc: string) => {
   const set = new Set<string>();
 
   visit(mdast(doc), isMDXEsm, (node: MdxjsEsm) => {
-    if (node.value?.match(EXPORT_NAME_REGEX)) {
-      const [, name] = node.value.match(EXPORT_NAME_REGEX);
-      set.add(name);
+    // Once inside an mdxjsEsm node, we need to check for one or more declared exports within data.estree.body
+    // This is because single newlines \n are not considered as a new block, so there may be more than one export statement in a single mdxjsEsm node
+    const body = node.data?.estree.body;
+    if (!body) return;
+
+    for (const child of body) {
+        if (child.type === 'ExportNamedDeclaration') {
+        // There are three types of ExportNamedDeclaration that we need to consider: VariableDeclaration, FunctionDeclaration, ClassDeclaration
+        const declaration = child.declaration;
+        // FunctionDeclaration and ClassDeclaration have the same structure 
+        if (declaration.type !== 'VariableDeclaration') {
+          // Note: declaration.id.type is always 'Identifier' for FunctionDeclarations and ClassDeclarations
+          set.add(declaration.id.name);
+        }
+        else  {
+          const declarations = declaration.declarations;
+          for (const declaration of declarations) {
+            const id = declaration.id;
+            if (id.type === 'Identifier') {
+              set.add(id.name);
+            }  
+          }
+        }
+      }
     }
+    
   });
 
   return Array.from(set);
 };
 
 export default exports;
+
+/* Example mdast structures to find first export name in a mdxjsEsm node:
+There are three types of export declarations that we need to consider:
+1. VARIABLE DECLARATION
+      "type": "mdxjsEsm",
+      "value": "export const Foo = () => <div>Hello world</div>\nexport const Bar = () => <div>hello darkness my old friend</div>",
+      "data": {
+        "estree": {
+          "type": "Program",
+          "body": [
+            {
+              "type": "ExportNamedDeclaration",
+              "declaration": {
+                "type": "VariableDeclaration",
+                "declarations": [
+                  {
+                    "type": "VariableDeclarator",
+                    "id": {
+                      "type": "Identifier",
+                      "name": "Foo" // --------> This is the export name
+                    },
+                    ...
+
+2. FUNCTION DECLARATION & CLASS DECLARATION
+      "estree": {
+          "type": "Program",
+          "body": [
+            {
+              "type": "ExportNamedDeclaration",
+              "declaration": {
+                "type": "ClassDeclaration" | "FunctionDeclaration",
+                "id": {
+                  "type": "Identifier",
+                  "name": "Foo" // --------> This is the export name
+                },
+*/
