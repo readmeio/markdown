714807c0e731d50e70d8717b97d96fb8340559db
Kelly Joseph Price <kellyjosephprice@gmail.com>
2025-02-12 13:13:16 -0800

    fix: migrate

--- lib/migrate.ts
+++ lib/migrate.ts
@@ -2,7 +2,14 @@ import mdastV6 from './mdastV6';
 import mdx from './mdx';
 
 const migrate = (doc: string, { rdmd }): string => {
-  return mdx(mdastV6(doc, { rdmd })).replaceAll(/&#x20;/g, ' ');
+  return (
+    mdx(mdastV6(doc, { rdmd }))
+      .replaceAll(/&#x20;/g, ' ')
+      // @note: I'm not sure what's happening, but I think mdx is converting an
+      // 'a' to '&#x61;' as a means of escaping it. I think this helps with
+      // parsing weird cases.
+      .replaceAll(/&#x61;/g, 'a')
+  );
 };
 
 export default migrate;
