5dc052df83daaa826750a84c653ed879f929632c
Kelly Joseph Price <kellyjosephprice@gmail.com>
2025-02-12 11:31:25 -0800

    feat: add tailwind to locally defined components (#1056)
    | [![PR App][icn]][demo] | Ref RM-11739 |
    | :--------------------: | :----------: |
    
    ## 🧰 Changes
    
    Adds tailwind support for locally defined components.
    
    ## 🧬 QA & Testing
    
    - [Broken on production][prod].
    - [Working in this PR app][demo].
    
    [demo]: https://markdown-pr-PR_NUMBER.herokuapp.com
    [prod]: https://SUBDOMAIN.readme.io
    [icn]:
    https://user-images.githubusercontent.com/886627/160426047-1bee9488-305a-4145-bb2b-09d8b757d38a.svg

--- lib/exports.ts
+++ lib/exports.ts
@@ -1,77 +1,9 @@
-import { visit } from 'unist-util-visit';
 import mdast from './mdast';
-import { isMDXEsm } from '../processor/utils';
-import { MdxjsEsm } from 'mdast-util-mdx';
-
-/* Example mdast structures to find first export name in a mdxjsEsm node:
-There are three types of export declarations that we need to consider:
-1. VARIABLE DECLARATION
-      "type": "mdxjsEsm",
-      "value": "export const Foo = () => <div>Hello world</div>\nexport const Bar = () => <div>hello darkness my old friend</div>",
-      "data": {
-        "estree": {
-          "type": "Program",
-          "body": [
-            {
-              "type": "ExportNamedDeclaration",
-              "declaration": {
-                "type": "VariableDeclaration",
-                "declarations": [
-                  {
-                    "type": "VariableDeclarator",
-                    "id": {
-                      "type": "Identifier",
-                      "name": "Foo" // --------> This is the export name
-                    },
-                    ...
-
-2/3. FUNCTION DECLARATION & CLASS DECLARATION
-      "estree": {
-          "type": "Program",
-          "body": [
-            {
-              "type": "ExportNamedDeclaration",
-              "declaration": {
-                "type": "ClassDeclaration" | "FunctionDeclaration",
-                "id": {
-                  "type": "Identifier",
-                  "name": "Foo" // --------> This is the export name
-                },
-*/
+import { getExports } from '../processor/utils';
 
 const exports = (doc: string) => {
-  const set = new Set<string>();
-
-  visit(mdast(doc), isMDXEsm, (node: MdxjsEsm) => {
-    // Once inside an mdxjsEsm node, we need to check for one or more declared exports within data.estree.body
-    // This is because single newlines \n are not considered as a new block, so there may be more than one export statement in a single mdxjsEsm node
-    const body = node.data?.estree.body;
-    if (!body) return;
-
-    for (const child of body) {
-        if (child.type === 'ExportNamedDeclaration') {
-        // There are three types of ExportNamedDeclaration that we need to consider: VariableDeclaration, FunctionDeclaration, ClassDeclaration
-        const declaration = child.declaration;
-        // FunctionDeclaration and ClassDeclaration have the same structure 
-        if (declaration.type !== 'VariableDeclaration') {
-          // Note: declaration.id.type is always 'Identifier' for FunctionDeclarations and ClassDeclarations
-          set.add(declaration.id.name);
-        }
-        else  {
-          const declarations = declaration.declarations;
-          for (const declaration of declarations) {
-            const id = declaration.id;
-            if (id.type === 'Identifier') {
-              set.add(id.name);
-            }  
-          }
-        }
-      }
-    }
-    
-  });
-
-  return Array.from(set);
+  return getExports(mdast(doc));
 };
 
 export default exports;
+
