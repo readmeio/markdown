f961c198dbf0ddcfdbcfe77a330a4b117931954e
Kelly Joseph Price <kellyjosephprice@gmail.com>
2025-02-12 10:59:00 -0800

    feat: add tailwind to locally defined components

--- processor/transform/tailwind.tsx
+++ processor/transform/tailwind.tsx
@@ -1,11 +1,12 @@
 import { PhrasingContent, BlockContent, Root } from 'mdast';
 import { Plugin } from 'unified';
+import { VFile } from 'vfile';
 
 import { MdxjsEsm, MdxJsxFlowElement, MdxJsxTextElement } from 'mdast-util-mdx';
 import { visit, SKIP } from 'unist-util-visit';
 import { valueToEstree } from 'estree-util-value-to-estree';
 
-import { hasNamedExport, isMDXElement, toAttributes } from '../utils';
+import { getExports, hasNamedExport, isMDXElement, toAttributes } from '../utils';
 import tailwindBundle from '../../utils/tailwind-bundle';
 
 interface TailwindRootOptions {
@@ -20,6 +21,8 @@ const exportTailwindStylesheet = async (tree: Root, components: TailwindRootOpti
   if (hasNamedExport(tree, 'stylesheet')) return;
 
   const stylesheet = (await tailwindBundle(Object.values(components).join('\n\n'), { prefix: `.readme-tailwind` })).css;
+
+  console.log(stylesheet);
 
   const exportNode: MdxjsEsm = {
     type: 'mdxjsEsm',
@@ -78,10 +81,15 @@ const injectTailwindRoot =
 
 const tailwind: Plugin<[TailwindRootOptions]> =
   ({ components }) =>
-  async (tree: Root) => {
-    visit(tree, isMDXElement, injectTailwindRoot({ components }));
+  async (tree: Root, vfile: VFile) => {
+    const localComponents = getExports(tree).reduce((acc, name) => {
+      acc[name] = String(vfile);
+      return acc;
+    }, {});
 
-    await exportTailwindStylesheet(tree, components);
+    visit(tree, isMDXElement, injectTailwindRoot({ components: { ...components, ...localComponents } }));
+
+    await exportTailwindStylesheet(tree, { ...components, ...localComponents });
 
     return tree;
   };
