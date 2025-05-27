eb668f6dadbbbb06d5b18731782ff2734169fb28
Kelly Joseph Price <kellyjosephprice@gmail.com>
2025-05-27 16:49:08 -0700

    test: more tests

--- __tests__/compilers/plain.test.ts
+++ __tests__/compilers/plain.test.ts
@@ -1,4 +1,4 @@
-import type { Paragraph, Root } from 'mdast';
+import type { Paragraph, Root, RootContent } from 'mdast';
 
 import { mdx } from '../../index';
 
@@ -21,5 +21,61 @@ describe('plain compiler', () => {
     };
 
     expect(mdx(ast)).toBe(`${md}\n`);
+  });
+
+  it('compiles plain nodes and does not escape characters', () => {
+    const md = '<not valid jsx>';
+    const ast: Root = {
+      type: 'root',
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            {
+              type: 'plain',
+              value: md,
+            },
+          ],
+        } as Paragraph,
+      ],
+    };
+
+    expect(mdx(ast)).toBe(`${md}\n`);
+  });
+
+  it('compiles plain nodes at the root level', () => {
+    const md = "- this is and isn't a list";
+    const ast: Root = {
+      type: 'root',
+      children: [
+        {
+          type: 'plain',
+          value: md,
+        },
+      ] as RootContent[],
+    };
+
+    expect(mdx(ast)).toBe(`${md}\n`);
+  });
+
+  it('compiles plain nodes in an inline context', () => {
+    const ast: Root = {
+      type: 'root',
+      children: [
+        {
+          type: 'paragraph',
+          children: [
+            { type: 'text', value: 'before' },
+            {
+              type: 'plain',
+              value: ' plain ',
+            },
+            { type: 'text', value: 'after' },
+          ],
+        },
+      ] as RootContent[],
+    };
+
+    expect(mdx(ast)).toBe('before plain after\n');
   });
 });
