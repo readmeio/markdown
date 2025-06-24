618687fbd7a0e7a746cd5799e09b4fe2416fb602
Rafe Goldberg <rafegoldberg@gmail.com>
2025-06-24 14:54:36 -0400

    chore(components): add PostmanRunButton (#1136)
    ## 🧰 Changes
    
    - [x] add the `<PostmanRunButton>` to our internal components[^1] so our
    users can easily insert it via the editor
    - [ ] make sure this component is usable in the editor, but is *not*
    displayed in the slash menu[^2]
    
    [^1]: duped from [the version in our
    Marketplace](https://github.com/readmeio/marketplace/tree/main/components/PostmanRunButton)
    [^2]: may need your expertise on this one @kellyjosephprice once this
    hits in ReadMe proper!

--- components/index.ts
+++ components/index.ts
@@ -17,3 +17,4 @@ export { default as Tabs, Tab } from './Tabs';
 export { default as TailwindRoot } from './TailwindRoot';
 export { default as TailwindStyle } from './TailwindStyle';
 export { default as TutorialTile } from './TutorialTile';
+export { default as PostmanRunButton } from './PostmanRunButton';
