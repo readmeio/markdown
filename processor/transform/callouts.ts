cc5e9c649e7b34317aeadbf73c7ebfeaef2d197b
Rafe Goldberg <rafegoldberg@gmail.com>
2025-07-31 11:01:43 -0400

    chore(transformer/callouts): add optional chain for start position too
    per https://github.com/readmeio/markdown/pull/1165#discussion_r2245595333

--- processor/transform/callouts.ts
+++ processor/transform/callouts.ts
@@ -31,7 +31,7 @@ export const wrapHeading = (node: Blockquote | Callout): Heading => {
     depth: 3,
     children: ('children' in firstChild ? firstChild.children : []) as Heading['children'],
     position: {
-      start: findFirst(firstChild).position.start,
+      start: findFirst(firstChild)?.position.start,
       end: findLast(firstChild)?.position.end,
     },
   };
