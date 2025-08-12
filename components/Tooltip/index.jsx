af63ec4f57c4a22f67be9e723a84d6401f645193
Asha Camper Singh <asha@readme.io>
2025-08-12 14:31:07 -0700

    fix: port glossary tooltip fix to `markdown-legacy`  (#1174)
    [![PR App][icn]][demo] | Fix CX-2117
    :-------------------:|:----------:
    
    ## 🧰 Changes
    
    Port the changes from #1173 over to the `markdown-legacy` branch /
    package
    
    ## 🧬 QA & Testing
    
    ### Before
    
    
    https://github.com/user-attachments/assets/479c7668-62bb-4657-a291-3b2274b793f8
    
    ### After
    
    
    https://github.com/user-attachments/assets/a809e6d2-7571-473c-b7f6-c854cff3f4f2
    
    
    - [Broken on production][prod].
    - [Working in this PR app][demo].
    
    
    [demo]: https://markdown-pr-PR_NUMBER.herokuapp.com
    [prod]: https://SUBDOMAIN.readme.io
    [icn]:
    https://user-images.githubusercontent.com/886627/160426047-1bee9488-305a-4145-bb2b-09d8b757d38a.svg

---
+++ components/Tooltip/index.jsx
@@ -0,0 +1,59 @@
+const Tippy = require('@tippyjs/react').default;
+const React = require('react');
+
+/**
+ * A custom wrapper around `@tippyjs/react` that attaches a MutationObserver to
+ * listen for changes of the trigger element's DOM tree.
+ *
+ * This is necessary for features like translation, where we initially render a
+ * Glossary term in English but then replace it with the translated term, which
+ * creates a stale Tippy reference.
+ */
+export default function Tooltip({ children, ...rest }) {
+  const triggerRef = React.useRef(null);
+  const [triggerTarget, setTriggerTarget] = React.useState(null);
+
+  const triggerId = React.useMemo(() => {
+    return `tooltip-trigger-${crypto.randomUUID()}`;
+  }, []);
+
+  React.useEffect(() => {
+    if (!triggerRef.current?.parentElement) return () => {};
+
+    const observer = new MutationObserver(records => {
+      records.forEach(record => {
+        // was the node tree changed?
+        if (record.type === 'childList') {
+          record.addedNodes.forEach(node => {
+            // was the node for the tooltip trigger replaced?
+            if (node instanceof HTMLElement && node.id === triggerId) {
+              // if it was, update our reference to it
+              setTriggerTarget(node);
+            }
+          });
+        }
+      });
+    });
+
+    observer.observe(triggerRef.current.parentElement, {
+      subtree: true,
+      childList: true,
+    });
+
+    return () => observer.disconnect();
+  }, [setTriggerTarget, triggerId, triggerRef, triggerTarget]);
+
+  const getReferenceClientRect = React.useMemo(() => {
+    return triggerTarget ? () => triggerTarget.getBoundingClientRect() : undefined;
+  }, [triggerTarget]);
+
+  return (
+    <Tippy getReferenceClientRect={getReferenceClientRect} triggerTarget={triggerTarget} {...rest}>
+      <span ref={triggerRef} id={triggerId}>
+        {children}
+      </span>
+    </Tippy>
+  );
+}
+
+Tooltip.propTypes = Tippy.propTypes;
