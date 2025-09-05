a3681e30f3b909230c85603be1685d727a6f6588
Kevin Ports <kevin.ports@gmail.com>
2025-09-05 10:43:13 -0500

    fix: update glossary to use tooltip with mutation observer (#1188)
    [![PR App][icn]][demo] | Fix CX-2117
    :-------------------:|:----------:
    
    
    https://github.com/user-attachments/assets/ce7e25ff-17d7-43c5-b9e2-9b4e939a5ab3
    
    
    
    We saw issues after releasing our [initial
    fix](https://github.com/readmeio/markdown/pull/1173) where the paragraph
    surrounding the tooltip would lose it's translation:
    
    <img width="400" alt="image"
    src="https://github.com/user-attachments/assets/10d70619-3d54-4a77-beb7-7c128f42fd47"
    />
    
    I believe this happened because our initial fix changed the underlying
    DOM structure by adding an extra span to identify the tippy trigger
    element:
    
    https://github.com/readmeio/markdown/blob/765dfc1809bdbafc3060c6c762335f229b33e3b4/components/Tooltip/index.tsx#L58
    
    and when we modify the DOM structure like that it can cause Transifex
    Live to see a different translation source, so the old translation no
    longer matches and that paragraph “loses” its translation (it becomes a
    new, untranslated string). Other paragraphs remain intact because their
    fingerprints didn’t change.
    
    This PR re-applies our initial MutationObserver fix but takes a
    different approach that doesn't modify the underlying DOM structure of
    he `GlossaryItem` component.
    
    ## 🧰 Changes
    - Attaches a mutation observer to the glossary tooltip trigger element
    - Adds mutation logic so that when something like translations blows
    away its DOM tree, it can get an updated reference to the new trigger
    node
    - This logic compares the signatures of the mutation's previous &
    updated nodes to ensure the tooltip trigger is in the same location in
    the tree
    - Then finds the tooltip trigger and re-attaches it to the tippy
    instance.
    
    ## 🧬 QA & Testing
    
    - [Broken on production][prod].
    - [Working in this PR app][demo].
    
    
    [demo]: https://markdown-pr-PR_NUMBER.herokuapp.com
    [prod]: https://SUBDOMAIN.readme.io
    [icn]:
    https://user-images.githubusercontent.com/886627/160426047-1bee9488-305a-4145-bb2b-09d8b757d38a.svg

--- __tests__/GlossaryItem.test.jsx
+++ __tests__/GlossaryItem.test.jsx
@@ -2,6 +2,7 @@ const { render, fireEvent, screen } = require('@testing-library/react');
 const React = require('react');
 
 const { GlossaryItem } = require('../components/GlossaryItem');
+const { nodeListSignature, compareNodeListSignatures } = require('../components/GlossaryItem/utils');
 
 test('should output a glossary item if the term exists', () => {
   const term = 'acme';
@@ -33,4 +34,48 @@ test('should output the term if the definition does not exist', () => {
 
   expect(container.querySelector('.GlossaryItem-trigger')).not.toBeInTheDocument();
   expect(container.querySelector('span')).toHaveTextContent(term);
+});
+
+test('nodeListSignature should return a stable signature for a list of nodes', () => {
+  const { container } = render(
+    <>
+      Text
+      <span>Text</span>
+      <GlossaryItem term="acme" terms={[{ term: 'acme', definition: 'This is a definition' }]} />
+      Text
+    </>,
+  );
+
+  const signature = nodeListSignature(container.childNodes);
+  expect(signature).toStrictEqual(['#text', 'span', 'span[class=GlossaryItem-trigger]', '#text']);
+});
+
+test('compareNodeListSignatures should determine if two node lists are structurally equivalent', () => {
+  const { container: containerA } = render(
+    <>
+      Text
+      <span>Text</span>
+      <GlossaryItem term="acme" terms={[{ term: 'acme', definition: 'This is a definition' }]} />
+      Text
+    </>,
+  );
+  const { container: containerB } = render(
+    <>
+      Text
+      <span>Text</span>
+      <GlossaryItem term="acme" terms={[{ term: 'acme', definition: 'This is a definition' }]} />
+      Text
+    </>,
+  );
+  const { container: containerC } = render(
+    <>
+      Text
+      <span className="I_AM_DIFFERENT">Text</span>
+      <GlossaryItem term="acme" terms={[{ term: 'acme', definition: 'This is a definition' }]} />
+      Text
+    </>,
+  );
+
+  expect(compareNodeListSignatures(containerA.childNodes, containerB.childNodes)).toBe(true);
+  expect(compareNodeListSignatures(containerA.childNodes, containerC.childNodes)).toBe(false);
 });
