e842f86a3e16e8e56a1729caf6dff29af03f768d
Kelly Joseph Price <kellyjosephprice@gmail.com>
2025-09-16 13:49:58 -0700

    chore: more tests

--- __tests__/migration/image.test.ts
+++ __tests__/migration/image.test.ts
@@ -1,7 +1,19 @@
 import { migrate } from '../helpers';
 
 describe('migrating images', () => {
-  it('compiles images', () => {
+  it('migrates markdown images', () => {
+    const md = `
+![now with alt text!](https://fastly.picsum.photos/id/507/200/300.jpg?hmac=v0NKvUrOWTKZuZFmMlLN_7-RdRgeF-qFLeBGXpufxgg)
+`;
+
+    const mdx = migrate(md);
+    expect(mdx).toMatchInlineSnapshot(`
+      "![now with alt text!](https://fastly.picsum.photos/id/507/200/300.jpg?hmac=v0NKvUrOWTKZuZFmMlLN_7-RdRgeF-qFLeBGXpufxgg)
+      "
+    `);
+  });
+
+  it('migrates magic block images', () => {
     const md = `
 [block:image]
 {
@@ -23,6 +35,35 @@ describe('migrating images', () => {
     const mdx = migrate(md);
     expect(mdx).toMatchInlineSnapshot(`
       "<Image alt="now with alt text!" align="center" className="border" border={true} src="https://fastly.picsum.photos/id/507/200/300.jpg?hmac=v0NKvUrOWTKZuZFmMlLN_7-RdRgeF-qFLeBGXpufxgg" />
+      "
+    `);
+  });
+
+  it('migrates magic block images with captions', () => {
+    const md = `
+[block:image]
+{
+  "images": [
+    {
+      "image": [
+        "https://fastly.picsum.photos/id/507/200/300.jpg?hmac=v0NKvUrOWTKZuZFmMlLN_7-RdRgeF-qFLeBGXpufxgg",
+        "",
+        "now with alt text!"
+      ],
+      "align": "center",
+      "border": true,
+      "caption": "This is a caption"
+    }
+  ]
+}
+[/block]
+`;
+
+    const mdx = migrate(md);
+    expect(mdx).toMatchInlineSnapshot(`
+      "<Image alt="now with alt text!" align="center" border={true} src="https://fastly.picsum.photos/id/507/200/300.jpg?hmac=v0NKvUrOWTKZuZFmMlLN_7-RdRgeF-qFLeBGXpufxgg">
+        This is a caption
+      </Image>
       "
     `);
   });
