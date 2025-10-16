5a7ae0362225fb1ecf18c2675b3240ddcead8193
eagletrhost <dimazanugrah12@gmail.com>
2025-10-17 10:48:40 +1100

    feat: move <br> regex to tree traversal & add test

--- lib/migrate.ts
+++ lib/migrate.ts
@@ -1,4 +1,5 @@
 import migrateCallouts from '../processor/transform/migrate-callouts';
+import migrateHtmlTags from '../processor/transform/migrate-html-tags';
 import migrateLinkReferences from '../processor/transform/migrate-link-references';
 
 import mdastV6 from './mdastV6';
@@ -8,14 +9,12 @@ const migrate = (doc: string, { rdmd }): string => {
   const ast = mdastV6(doc, { rdmd });
 
   return (
-    mdx(ast, { remarkTransformers: [migrateCallouts, migrateLinkReferences], file: doc })
+    mdx(ast, { remarkTransformers: [migrateCallouts, migrateLinkReferences, migrateHtmlTags], file: doc })
       .replaceAll(/&#x20;/g, ' ')
       // @note: I'm not sure what's happening, but I think mdx is converting an
       // 'a' to '&#x61;' as a means of escaping it. I think this helps with
       // parsing weird cases.
       .replaceAll(/&#x61;/g, 'a')
-      // A common issue is that <br> tags are not properly closed
-      .replaceAll(/<br(?!\s*\/)\s*>/g, '<br />')
   );
 };
 
---
+++ processor/transform/migrate-html-tags.ts
@@ -0,0 +1,14 @@
+import type { Root } from 'mdast';
+
+import { visit } from 'unist-util-visit';
+
+// Add more visits to migrate other HTML tags here
+const migrateHtmlTags = () => (tree: Root) => {
+  // A common issue is that <br> tags are not properly closed
+  // HTML tags like <br> are parsed as 'html' nodes
+  visit(tree, 'html', htmlNode => {
+    htmlNode.value = htmlNode.value.replaceAll(/<br(?!\s*\/>)([^>]*?)>/g, '<br$1 />');
+  });
+};
+
+export default migrateHtmlTags;
