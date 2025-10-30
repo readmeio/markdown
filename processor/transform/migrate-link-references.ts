7547439a144c92340de240ac44b4694aca703c83
eagletrhost <dimazanugrah12@gmail.com>
2025-10-30 17:05:08 +1100

    fix: modify migrate link reference to not accidentally replace [] values

--- processor/transform/migrate-link-references.ts
+++ processor/transform/migrate-link-references.ts
@@ -1,19 +1,18 @@
 import type { LinkReference, Parents, Root } from 'mdast';
 import type { Plugin } from 'unified';
-import type { VFile } from 'vfile';
 
 import { visit } from 'unist-util-visit';
 
 import { NodeTypes } from '../../enums';
 
 const migrateLinkReferences: Plugin<[], Root> = () => {
-  return (tree: Root, vfile: VFile) => {
+  return (tree: Root) => {
     visit(tree, 'linkReference', (node: LinkReference, index: number, parent: Parents) => {
       if (!('children' in parent)) return;
 
       parent.children.splice(index, 1, {
         type: NodeTypes.plain,
-        value: String(vfile).slice(node.position.start.offset, node.position.end.offset),
+        value: `[${node.label}]`,
       });
     });
   };
