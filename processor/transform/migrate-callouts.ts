f69aeae1138871303f4997d0818d53c8f3def052
eagletrhost <dimazanugrah12@gmail.com>
2025-11-10 14:34:19 +1100

    fix: skip wrapping heading in callouts for non pragraph

--- processor/transform/migrate-callouts.ts
+++ processor/transform/migrate-callouts.ts
@@ -7,6 +7,10 @@ import { wrapHeading } from './callouts';
 
 const migrateCallouts: Plugin<[], Root> = (): Transformer<Root> => (tree: Root) => {
   visit(tree, 'rdme-callout', callout => {
+    const firstChild = callout.children?.[0];
+    // This will retain the value of the node if it is not a paragraph, e.g. an HTML node
+    if (!firstChild || firstChild.type !== 'paragraph') return;
+
     callout.children[0] = wrapHeading(callout);
   });
 
