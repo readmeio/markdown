4e9de4b05bdbc1cffb5c071e123009b17213ab1b
eagletrhost <dimazanugrah12@gmail.com>
2025-11-11 12:17:07 +1100

    test: add empty callout test

--- __tests__/migration/callouts.test.ts
+++ __tests__/migration/callouts.test.ts
@@ -1,6 +1,15 @@
 import { migrate } from '../helpers';
 
 describe('migrating callouts', () => {
+  it('does not error on callouts with no heading', () => {
+    const md = '> ℹ️';
+    const mdx = migrate(md);
+    expect(mdx).toMatchInlineSnapshot(`
+      "<Callout icon="ℹ️" theme="info" />
+      "
+    `);
+  });
+
   it('retains HTML content that starts a callout body', () => {
     const md = `> ⚠️ <div><b>hello</b></div>
     `;
