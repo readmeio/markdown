60691dc5b1cbc95dff94f27b9d2c15134abfd3ba
Dimas Putra Anugerah <63914983+eaglethrost@users.noreply.github.com>
2025-11-11 12:23:54 +1100

    feat: migrate contents inside comments (#1229)
    [![PR App][icn]][demo] | Fix RM-XYZ
    :-------------------:|:----------:
    
    ## 🧰 Changes
    
    In the migrate function, recursively call the base migrate on contents
    inside comment blocks
    
    ## 🧬 QA & Testing
    
    - [Broken on production][prod].
    - [Working in this PR app][demo].
    
    
    [demo]: https://markdown-pr-PR_NUMBER.herokuapp.com
    [prod]: https://SUBDOMAIN.readme.io
    [icn]:
    https://user-images.githubusercontent.com/886627/160426047-1bee9488-305a-4145-bb2b-09d8b757d38a.svg

--- lib/migrate.ts
+++ lib/migrate.ts
@@ -4,18 +4,23 @@ import migrateLinkReferences from '../processor/transform/migrate-link-reference
 
 import mdastV6 from './mdastV6';
 import mdx from './mdx';
+import migrateComments from './utils/migrateComments';
 
-const migrate = (doc: string, { rdmd }): string => {
+const migrateDoc = (doc: string, { rdmd }): string => {
   const ast = mdastV6(doc, { rdmd });
 
-  return (
-    mdx(ast, { remarkTransformers: [migrateCallouts, [migrateLinkReferences, { rdmd }], migrateHtmlTags], file: doc })
-      .replaceAll(/&#x20;/g, ' ')
-      // @note: I'm not sure what's happening, but I think mdx is converting an
-      // 'a' to '&#x61;' as a means of escaping it. I think this helps with
-      // parsing weird cases.
-      .replaceAll(/&#x61;/g, 'a')
-  );
+  return mdx(ast, { remarkTransformers: [migrateCallouts, [migrateLinkReferences, { rdmd }], migrateHtmlTags], file: doc })
+    .replaceAll(/&#x20;/g, ' ')
+    // @note: I'm not sure what's happening, but I think mdx is converting an
+    // 'a' to '&#x61;' as a means of escaping it. I think this helps with
+    // parsing weird cases.
+    .replaceAll(/&#x61;/g, 'a');
+};
+
+const migrate = (doc: string, opts): string => {
+  const migratedDoc = migrateDoc(doc, opts);
+  const migratedDocAndComments = migrateComments(migratedDoc, migrateDoc, opts);
+  return migratedDocAndComments;
 };
 
 export default migrate;
