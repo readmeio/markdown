b93cc2814f3175cac01e1132e9013640c8e40108
Tony Li <runnabro@users.noreply.github.com>
2025-12-09 15:42:35 -0800

    comments

--- components/Cards/index.tsx
+++ components/Cards/index.tsx
@@ -11,35 +11,34 @@ interface CardProps
     kind?: 'card' | 'tile';
     target?: string;
     title?: string;
-  }>{}
+  }> {}
 
 export const Card = ({ badge, children, href, kind = 'card', icon, iconColor, target, title }: CardProps) => {
   const Tag = href ? 'a' : 'div';
 
-    return (
+  return (
     <Tag className={`rm-Card rm-Card_${kind}`} href={href} target={target}>
       {icon && <i className={`rm-Card-icon fa-duotone fa-solid ${icon}`} style={{ '--Card-icon-color': iconColor } as React.CSSProperties}></i>}
-        <div className="rm-Card-content">
-          {title && (
-            <div className="rm-Card-title">
-              {title}
-              {badge && <span className="rm-Card-badge">{badge}</span>}
-              {href && <i aria-hidden="true" className="rm-Card-arrow fa-regular fa-arrow-right" />}
-            </div>
-          )}
-          {children}
-        </div>
+      <div className="rm-Card-content">
+        {title && (
+          <div className="rm-Card-title">
+            {title}
+            {badge && <span className="rm-Card-badge">{badge}</span>}
+            {href && <i aria-hidden="true" className="rm-Card-arrow fa-regular fa-arrow-right" />}
+          </div>
+        )}
+        {children}
+      </div>
     </Tag>
   );
 };
 
-interface CardsGripProps extends React.PropsWithChildren<{ columns?: number }> {}
+interface CardsGridProps extends React.PropsWithChildren<{ columns?: number | string, minWidth?: string }> {}
 
-const CardsGrid = ({ columns = 2, children }: CardsGripProps) => {
-  // eslint-disable-next-line no-param-reassign
-  columns = columns >= 2 ? columns : 2;
+const CardsGrid = ({ columns = 'auto-fit', children, minWidth = '150px' }: CardsGridProps) => {
+
   return (
-    <div className="rm-CardsGrid" style={{ '--CardsGrid-template-columns': columns } as React.CSSProperties}>
+    <div className="rm-CardsGrid" style={{ '--CardsGrid-columns': columns, '--CardsGrid-minWidth': minWidth } as React.CSSProperties}>
       {children}
     </div>
   );
