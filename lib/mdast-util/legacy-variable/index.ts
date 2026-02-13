df08d194422f24bca91fa80aaec6450cd24ec907
eagletrhost <dimazanugrah12@gmail.com>
2026-02-13 22:53:24 +1100

    feat: support legacy glossary to mdx text

--- lib/mdast-util/legacy-variable/index.ts
+++ lib/mdast-util/legacy-variable/index.ts
@@ -1,4 +1,5 @@
 import type { CompileContext, Extension as FromMarkdownExtension, Handle, Token } from 'mdast-util-from-markdown';
+import type { MdxJsxTextElement } from 'mdast-util-mdx-jsx';
 import type { Variable } from 'types';
 
 import { NodeTypes } from '../../../enums';
@@ -30,26 +31,73 @@ function exitlegacyVariableValue(this: CompileContext, token: Parameters<Handle>
   if (ctx) ctx.value += this.sliceSerialize(token);
 }
 
+function createGlossaryNode(value: string, token: Parameters<Handle>[0]): MdxJsxTextElement {
+  const term = value.slice('glossary:'.length).trim();
+  const glossaryPrependLength = '<<glossary:'.length;
+  const closingArrowLength = '>>'.length;
+  return {
+    type: 'mdxJsxTextElement',
+    name: 'Glossary',
+    attributes: [],
+    children: [{
+      type: 'text',
+      value: term,
+      position: {
+        start: {
+          offset: token.start.offset + glossaryPrependLength,
+          line: token.start.line,
+          column: token.start.column + glossaryPrependLength,
+        },
+        end: {
+          offset: token.end.offset - closingArrowLength,
+          line: token.end.line,
+          column: token.end.column - closingArrowLength,
+        },
+      },
+    }],
+    position: {
+      start: {
+        offset: token.start.offset,
+        line: token.start.line,
+        column: token.start.column,
+      },
+      end: {
+        offset: token.end.offset,
+        line: token.end.line,
+        column: token.end.column,
+      },
+    },
+  };
+}
+
 function exitlegacyVariable(this: CompileContext, token: Parameters<Handle>[0]): void {
   const ctx = contextMap.get(token);
   const serialized = this.sliceSerialize(token);
-  const raw =
+  const rawValue =
     serialized.startsWith('<<') && serialized.endsWith('>>')
       ? serialized.slice(2, -2)
       : ctx?.value ?? '';
-  const trimmed = raw.trim();
+  const trimmed = rawValue.trim();
 
-  this.enter(
-    {
-      type: NodeTypes.variable,
-      data: {
-        hName: 'Variable',
-        hProperties: { name: trimmed },
-      },
-      value: `<<${raw}>>`,
-    } as Variable,
-    token,
-  );
+  if (trimmed.startsWith('glossary:')) {
+    // Since there isn't a Node type for Glossary anymore, we just parse it as an mdxJsxTextElement
+    // and let the pipeline later convert it to a component
+    // It's not truly an mdx text element, but we can treat it as such for now
+    const glossaryNode = createGlossaryNode(rawValue, token);
+    this.enter(glossaryNode, token);
+  } else {
+    this.enter(
+      {
+        type: NodeTypes.variable,
+        data: {
+          hName: 'Variable',
+          hProperties: { name: trimmed },
+        },
+        value: `<<${rawValue}>>`,
+      } as Variable,
+      token,
+    );
+  }
 
   this.exit(token);
   contextMap.delete(token);
