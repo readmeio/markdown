559eaa7b4d7911dd0e7067c3642185703580345a
eagletrhost <dimazanugrah12@gmail.com>
2026-02-13 17:18:04 +1100

    test: basic var test

--- __tests__/lib/mdxish/legacy-variables.test.ts
+++ __tests__/lib/mdxish/legacy-variables.test.ts
@@ -1,4 +1,4 @@
-import type { Element } from 'hast';
+import type { Element, Text } from 'hast';
 
 import { mdxish } from '../../../lib';
 
@@ -14,5 +14,39 @@ describe('legacy variables resolution', () => {
     const variableNode = (tree.children[0] as Element).children[0] as Element;
     expect(variableNode.tagName).toBe('variable');
     expect(variableNode.properties.name).toBe(varName);
+  });
+
+  it('should resolve inline <<>> to a variable node', () => {
+    const varName = 'email';
+    const md = `Hello <<${varName}>>!`;
+    const tree = mdxish(md);
+
+    expect(tree.children.length).toBeGreaterThanOrEqual(1);
+    const parent = tree.children[0] as Element;
+    expect(parent.children).toHaveLength(3);
+
+    const variableNode = parent.children[1] as Element;
+    expect(variableNode.tagName).toBe('variable');
+    expect(variableNode.properties.name).toBe(varName);
+
+    expect((parent.children[0] as Text).value).toBe('Hello ');
+    expect((parent.children[2] as Text).value).toBe('!');
+  });
+
+  it('should resolve <<>> that is surrounded by text', () => {
+    const varName = 'email';
+    const md = `abcd<<${varName}>>efg`;
+    const tree = mdxish(md);
+
+    expect(tree.children.length).toBeGreaterThanOrEqual(1);
+    const parent = tree.children[0] as Element;
+    expect(parent.children).toHaveLength(3);
+
+    const variableNode = parent.children[1] as Element;
+    expect(variableNode.tagName).toBe('variable');
+    expect(variableNode.properties.name).toBe(varName);
+
+    expect((parent.children[0] as Text).value).toBe('abcd');
+    expect((parent.children[2] as Text).value).toBe('efg');
   });
 });
