42a3150aef01fdf50dcd8a59fc093b7b3e658bdc
Jadenzzz <94533693+Jadenzzz@users.noreply.github.com>
2026-03-20 18:28:34 +1100

    update type

--- types.d.ts
+++ types.d.ts
@@ -49,13 +49,18 @@ interface EmbedBlock extends Node {
     hName: 'embed';
     hProperties: {
       favicon?: string;
+      height?:string;
+      href?: string;
       html?: string;
       iframe?: boolean;
       image?: string;
+      provider?:string;
       providerName?: string;
       providerUrl?: string;
       title: string;
+      typeOfEmbed?: string;
       url: string;
+      width?: string;
     };
   };
   label?: string;
