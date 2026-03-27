5fb52150cc53a0acbec08765970ef98086e3d0b6
eagletrhost <dimazanugrah12@gmail.com>
2026-03-27 23:52:38 +1100

    test: flag test

--- __tests__/lib/compile.test.ts
+++ __tests__/lib/compile.test.ts
@@ -1,6 +1,20 @@
 import { compile } from '../../index';
 
 describe('compile', () => {
+  describe('hardBreaks', () => {
+    it('does not convert newlines to hard breaks by default', () => {
+      const md = 'hello\nthere';
+      const tree = compile(md);
+      expect(tree).not.toMatch(/br/);
+    });
+
+    it('converts newlines to hard breaks when enabled', () => {
+      const md = 'hello\nthere';
+      const tree = compile(md, { hardBreaks: true });
+      expect(tree).toMatch(/br/);
+    });
+  });
+
   describe("{ format: 'md' }", () => {
     it('returns plain text of markdown components', () => {
       const md = '[link to doc](doc:getting-started)';
