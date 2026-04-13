4303ed0f65c7b979bccd24e51b732b84784e6896
eagletrhost <dimazanugrah12@gmail.com>
2026-04-13 15:16:58 +1000

    test: Code

--- __tests__/components/Callout.test.tsx
+++ __tests__/components/Callout.test.tsx
@@ -12,28 +12,30 @@ import { mdxish } from '../../lib';
 import { renderingEngines } from './utils';
 
 describe('Callout', () => {
-  it('render _all_ its children', () => {
-    render(
-      <Callout icon="icon" theme="theme" title="Title">
-        <p>Title</p>
-        <p>First Paragraph</p>
-        <p>Second Paragraph</p>
-      </Callout>,
-    );
+  describe('general component rendering', () => {
+    it('render _all_ its children', () => {
+      render(
+        <Callout icon="icon" theme="theme" title="Title">
+          <p>Title</p>
+          <p>First Paragraph</p>
+          <p>Second Paragraph</p>
+        </Callout>,
+      );
 
-    expect(screen.getByText('Second Paragraph')).toBeVisible();
-  });
+      expect(screen.getByText('Second Paragraph')).toBeVisible();
+    });
 
-  it("doesn't render all its children if it's **empty**", () => {
-    render(
-      <Callout empty icon="icon" theme="theme">
-        <p>Title</p>
-        <p>First Paragraph</p>
-        <p>Second Paragraph</p>
-      </Callout>,
-    );
+    it("doesn't render all its children if it's **empty**", () => {
+      render(
+        <Callout empty icon="icon" theme="theme">
+          <p>Title</p>
+          <p>First Paragraph</p>
+          <p>Second Paragraph</p>
+        </Callout>,
+      );
 
-    expect(screen.queryByText('Title')).toBeNull();
+      expect(screen.queryByText('Title')).toBeNull();
+    });
   });
 
   it.each(renderingEngines)('%s: renders a callout with icon and body', (_label, renderContent) => {
