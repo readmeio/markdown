f9788a03d8b23fa850897236a4d6107bdcbb6a04
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2026-04-16 15:24:03 +1000

    feat: integrate new tokenizer into mdxish pipeline
    also removing the htmlblock preprocessing step

--- lib/mdxish.ts
+++ lib/mdxish.ts
@@ -58,10 +58,12 @@ import tailwindTransformer from '../processor/transform/tailwind';
 
 import { emptyTaskListItemFromMarkdown } from './mdast-util/empty-task-list-item';
 import { gemojiFromMarkdown } from './mdast-util/gemoji';
+import { htmlBlockComponentFromMarkdown } from './mdast-util/html-block-component';
 import { jsxTableFromMarkdown } from './mdast-util/jsx-table';
 import { legacyVariableFromMarkdown } from './mdast-util/legacy-variable';
 import { magicBlockFromMarkdown } from './mdast-util/magic-block';
 import { gemoji } from './micromark/gemoji';
+import { htmlBlockComponent } from './micromark/html-block-component';
 import { jsxComment } from './micromark/jsx-comment';
 import { jsxTable } from './micromark/jsx-table';
 import { legacyVariable } from './micromark/legacy-variable';
@@ -156,8 +158,9 @@ export function mdxishAstProcessor(mdContent: string, opts: MdxishOpts = {}) {
     text: mdxExprExt.text,
   };
 
-  const micromarkExts = [jsxTable(), magicBlock(), gemoji(), legacyVariable(), looseHtmlEntity()];
+  const micromarkExts = [htmlBlockComponent(), jsxTable(), magicBlock(), gemoji(), legacyVariable(), looseHtmlEntity()];
   const fromMarkdownExts = [
+    htmlBlockComponentFromMarkdown(),
     jsxTableFromMarkdown(),
     magicBlockFromMarkdown(),
     gemojiFromMarkdown(),
