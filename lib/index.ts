fc6c6f4f07468f954ff8ce32692e540c2d2d3e3a
Maximilian Falco Widjaya <maximilianfalco@gmail.com>
2026-06-22 23:36:49 +1000

    fix: export extractToc via lib wrapper to maintain consistency

--- lib/index.ts
+++ lib/index.ts
@@ -11,7 +11,7 @@ export { default as mdx } from './mdx';
 export { default as mix } from './mix';
 export { default as mdxish, mdxishAstProcessor, mdxishMdastToMd } from './mdxish';
 export type { MdxishOpts } from './mdxish';
-export { extractToc } from '../processor/plugin/toc';
+export { default as extractToc } from './extractToc';
 export { default as migrate } from './migrate';
 export { default as plain } from './plain';
 export { default as renderMdxish } from './renderMdxish';
